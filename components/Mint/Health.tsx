fdc6ee40eae5dc3a93ba8668cd2702a79cddc665
triccs <105668529+triccs@users.noreply.github.com>
2024-04-08 16:00:01 -0400

    Fix Health

--- components/Mint/Health.tsx
+++ components/Mint/Health.tsx
@@ -7,9 +7,9 @@ import { num } from '@/helpers/num'
 type Props = {}
 
 const Health = (props: Props) => {
-  const { ltv, borrowLTV } = useVaultSummary()
+  const { ltv, liqudationLTV } = useVaultSummary()
 
-  const health = num(1).minus(num(ltv).dividedBy(borrowLTV)).times(100).dp(0).toNumber()
+  const health = num(1).minus(num(ltv).dividedBy(liqudationLTV)).times(100).dp(0).toNumber()
   return (
     <Box position="absolute" bottom="227px" left="110px">
       <Text fontSize="20px" fontWeight="bold" color="black">

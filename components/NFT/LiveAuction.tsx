7180e68815712b73faf1097293bbd76ef10fda21
Trix <triccs333@gmail>
2024-04-12 13:46:48 -0400

    Test Thirdweb

--- components/NFT/LiveAuction.tsx
+++ components/NFT/LiveAuction.tsx
@@ -2,13 +2,14 @@ import { Card, Image } from "@chakra-ui/react"
 import { MediaRenderer } from "@thirdweb-dev/react"
 import useIPFS from "./hooks/useLiveNFTAuction";
 
-
+//ipfs://bafybeibyujxdq5bzf7m5fadbn3vysh3b32fvontswmxqj6rxj5o6mi3wvy/0.png
+//ipfs://bafybeid2chlkhoknrlwjycpzkiipqypo3x4awnuttdx6sex3kisr3rgfsm
 const LiveAuction = () => {
     // useIPFS()
     return (
         <Card w="full" p="8" alignItems="center" gap={5} h="full" justifyContent="space-between">
             <MediaRenderer
-                src="ipfs://QmV4HC9fNrPJQeYpbW55NLLuSBMyzE11zS1L4HmL6Lbk7X"
+                src="ipfs://bafybeibyujxdq5bzf7m5fadbn3vysh3b32fvontswmxqj6rxj5o6mi3wvy/0.png"
                 alt="Current Auctioned NFT Image"
             // width="80%"
             // height="80%"

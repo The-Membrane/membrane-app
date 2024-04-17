211c0ad5bd83ae074872a1980a1db03c0673b0a3
Trix <triccs333@gmail>
2024-04-16 20:55:10 -0400

    revert

--- helpers/chain.ts
+++ helpers/chain.ts
@@ -1,4 +1,4 @@
-import { assets as registryAssets } from '@chain-registry/osmosis';
+import { assets as registryAssets } from 'chain-registry'
 import { Asset as RegistryAsset } from '@chain-registry/types'
 import { getExponentByDenom } from '@chain-registry/utils'
 import lpAssets from '@/config/lpAssets.json'
@@ -9,35 +9,33 @@ export type Asset = RegistryAsset & {
   isLP: boolean
 }
 
-// const defaultChain = 'osmosis'
+const defaultChain = 'osmosis'
 
-// export const getAssetLogo = (asset: RegistryAsset) => {
-//   return asset?.logo_URIs?.svg || asset?.logo_URIs?.png || asset?.logo_URIs?.jpeg
-// }
+export const getAssetLogo = (asset: RegistryAsset) => {
+  return asset?.logo_URIs?.svg || asset?.logo_URIs?.png || asset?.logo_URIs?.jpeg
+}
 
-// const assetWithLogo = (asset: RegistryAsset) => ({
-//   ...asset,
-//   logo: getAssetLogo(asset),
-//   decimal: getExponentByDenom(registryAssets, asset.base, defaultChain),
-//   isLP: false,
-// })
+const assetWithLogo = (asset: RegistryAsset) => ({
+  ...asset,
+  logo: getAssetLogo(asset),
+  decimal: getExponentByDenom(registryAssets, asset.base, defaultChain),
+  isLP: false,
+})
 
-// export const getChainAssets = () => {
-//   const chainAssets = registryAssets.find((asset) => asset.chain_name === defaultChain)
+export const getChainAssets = () => {
+  const chainAssets = registryAssets.find((asset) => asset.chain_name === defaultChain)
 
-//   const assetsWtihLogo = chainAssets?.assets?.map(assetWithLogo) || []
+  const assetsWtihLogo = chainAssets?.assets?.map(assetWithLogo) || []
 
-//   return [...assetsWtihLogo, ...lpAssets]
-// }
+  return [...assetsWtihLogo, ...lpAssets]
+}
 
 export const getAssetBySymbol = (symbol: string) => {
-  // const assets = getChainAssets()
-  const assets = registryAssets
+  const assets = getChainAssets()
   return assets?.find((asset) => asset.symbol === symbol)
 }
 
 export const getAssetByDenom = (denom: string) => {
-  // const assets = getChainAssets()
-  const assets = registryAssets
+  const assets = getChainAssets()
   return assets?.find((asset) => asset.base === denom)
 }

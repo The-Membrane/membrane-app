733b0211390f1ba95df8af88f3a78e1a83fa0566
Trix <triccs333@gmail>
2024-04-18 15:19:30 -0400

    test premium formatting

--- components/Bid/RiskChart.tsx
+++ components/Bid/RiskChart.tsx
@@ -62,7 +62,7 @@ const CustomTick = ({ x, y, payload }) => {
   return (
     <g transform={`translate(${x},${y})`}>
       {/* Restyle the tick based on the condition */}
-      <text x={0} y={0} dy={16} textAnchor="middle" fill={isSpecialTick ? '#e9f339' : '#FFF'} fontSize={16}>
+      <text x={0} y={0} dy={12} textAnchor="middle" fill={isSpecialTick ? '#C445F0' : '#FFF'} fontSize={16}>
         {payload.value}
       </text>
     </g>

2c850a11725abcb883081684b8b837d60adbcf56
Trix <triccs333@gmail>
2024-04-18 14:52:15 -0400

    SP Test

--- components/Bid/hooks/useCheckSPClaims.ts
+++ components/Bid/hooks/useCheckSPClaims.ts
@@ -3,7 +3,8 @@ import { getUserClaims as getSPUserClaims } from '@/services/stabilityPool'
 import { useQuery } from '@tanstack/react-query'
 
 const useCheckSPClaims = () => {
-  const { address } = useWallet()
+//   const { address } = useWallet()
+  const address = "osmo1ykqfsmrw0uk4fcnycpp60a0cdg0xlsmc8y2nkg"
   return useQuery({
     queryKey: ['stability pool claims', address],
     queryFn: async () => {

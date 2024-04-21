2780a45a4145922dc649ac887335691039fe352d
Trix <triccs333@gmail>
2024-04-21 15:34:29 -0400

    fix liquidationLTV calc for new positions

--- services/cdp.ts
+++ services/cdp.ts
@@ -205,7 +205,6 @@ export const getBorrowLTV = (
   positions: Positions[],
   basketAssets: BasketAsset[] = [],
 ) => {
-  console.log(positions, tvl)
   const positionsWithRatio = getAssetRatio(tvl, positions)
   const maxBorrowLTV = positionsWithRatio.reduce((acc, position) => {
     const ltv =
@@ -234,7 +233,7 @@ export const getLiqudationLTV = (
   const positionsWithRatio = getAssetRatio(tvl, positions)
 
   const maxLTV = positionsWithRatio.reduce((acc, position) => {
-    const ltv = basketAssets.find((asset) => asset?.asset?.base === position.denom)?.maxLTV || 0
+    const ltv = basketAssets.find((asset) => asset?.asset?.base === position.denom || asset?.asset?.base === position.base)?.maxLTV || 0
     return acc.plus(num(position.ratio).times(100).times(ltv))
   }, num(0))
   return maxLTV.toNumber()
@@ -287,7 +286,7 @@ const updatedSummary = (summary: any, basketPositions: any, prices: any) => {
 
   //If no initial position, return a summary using the summary from the mint state
   if (!basketPositions){
-    
+
     return summary.map((position) => {
       const price = prices?.find((p) => p.denom === position.base)?.price || 0
       const amount = num(position.amount).toNumber()

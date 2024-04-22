bf89fb2a007c0cfad789994cc21c07a6294a71dc
Trix <triccs333@gmail>
2024-04-22 12:40:16 -0400

    check proposal output

--- components/Governance/ProposalsTable.tsx
+++ components/Governance/ProposalsTable.tsx
@@ -106,6 +106,7 @@ const ProposalsTable = () => {
   const [filters, setFilters] = useState<Filter>({ status: 'active' })
   const [search, setSearch] = useState<string>('')
   const { data = [], isLoading } = useProposals()
+  console.log(data)
   const { paginatedData, ...pagination } = usePagination(data, 10, filters, search)
 
   if (isLoading) {

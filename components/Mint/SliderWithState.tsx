8e54661d4fe2ce041cb4edf5618678befce7e560
Trix <triccs333@gmail>
2024-04-25 13:14:26 -0400

    Test

--- components/Mint/SliderWithState.tsx
+++ components/Mint/SliderWithState.tsx
@@ -18,6 +18,16 @@ const handleThumbClick = (event) => {
 };
 
 export const SliderWithState = ({ value = 0, onChange, min = 0, max = 100, walletCDT = 0, summary = []}: SliderProps) => {
+
+  const sliderRef = React.useRef();
+
+  const handleChange = (value) => {
+    onChange(value);
+    // Handle slider value change here
+    // Optionally, you can set focus back to the slider after value change
+    sliderRef.current.focus();
+  };
+
   return (
     <Slider
       aria-label="slider-ex-4"
@@ -25,13 +35,13 @@ export const SliderWithState = ({ value = 0, onChange, min = 0, max = 100, walle
       min={min}
       max={max === 0 ? 1 : max}
       value={value}
-      onChange={onChange}
+      onChange={handleChange}
       isDisabled={max === 0 && (walletCDT === 0 || summary.length === 0)}
     >
       <SliderTrack bg="#E2D8DA" h="2" borderRadius="80px">
         <SliderFilledTrack bg="#C445F0" />
       </SliderTrack>
-      <SliderThumb boxSize={6} bg="#C445F0" cursor="grab" border="2px solid #E2D8DA" onChange={handleThumbClick} />
+      <SliderThumb boxSize={6} bg="#C445F0" cursor="grab" border="2px solid #E2D8DA"/>
     </Slider>
   )
 }

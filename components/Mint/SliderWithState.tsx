7f18a3708e6fad48541f715484b5230016900eb6
Trix <triccs333@gmail>
2024-04-25 13:25:00 -0400

    Test

--- components/Mint/SliderWithState.tsx
+++ components/Mint/SliderWithState.tsx
@@ -25,7 +25,7 @@ export const SliderWithState = ({ value = 0, onChange, min = 0, max = 100, walle
       <SliderTrack bg="#E2D8DA" h="2" borderRadius="80px">
         <SliderFilledTrack bg="#C445F0" />
       </SliderTrack>
-      <SliderThumb boxSize={6} bg="#C445F0" cursor="grab" border="2px solid #E2D8DA" />
+      <SliderThumb boxSize={6} bg="#C445F0" cursor="grab" border="2px solid #E2D8DA" autoFocus={false}/>
     </Slider>
   )
 }

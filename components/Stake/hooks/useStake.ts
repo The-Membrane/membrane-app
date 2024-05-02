fd39e1a94b6f18d4e72dc4ee06d76c8adc9d698e
Trix <triccs333@gmail>
2024-05-02 15:58:58 -0400

    change stake min amount to 1 so we can add lockdrop detail to the stake minimum error

--- components/Stake/hooks/useStake.ts
+++ components/Stake/hooks/useStake.ts
@@ -36,7 +36,7 @@ const useStakeing = ({}: UseStake) => {
 
       return [msg]
     },
-    enabled: !!address && !!mbrnAsset && !!contracts.staking && Number(amount) > 0,
+    enabled: !!address && !!mbrnAsset && !!contracts.staking && Number(amount) > 1,
   })
 
   return useSimulateAndBroadcast({

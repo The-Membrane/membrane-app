719a646afd5c23e1ee91f18b1182732d5a87d6a5
Trix <triccs333@gmail>
2024-05-07 13:58:22 -0400

    logs

--- components/Mint/AssetWithSlider.tsx
+++ components/Mint/AssetWithSlider.tsx
@@ -36,7 +36,7 @@ export const AssetWithSlider = ({ asset, label }: AssetWithSliderProps) => {
 
     setMintState({ assets: updatedAssets, summary, totalUsdValue })
   }
-  console.log("Asset in slider:", asset?)
+  console.log("Asset in slider:", asset)
   return (
     <Stack gap="0">
       <HStack justifyContent="space-between">

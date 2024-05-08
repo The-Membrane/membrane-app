7357d58a997d5766d2606cdffcd079a768f48bce
Trix <triccs333@gmail>
2024-05-08 14:28:27 -0400

    reposition minimum debt label

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -162,10 +162,10 @@ const Home = () => {
             QAState={quickActionState}
             onMenuChange={onMenuChange}
           />
+          <QuickActionLTVWithSlider label="Your Debt" value={sliderValue}/>
           { maxMint < 100 ? <Text fontSize="sm" color="red.500" mt="2" minH="21px">
              Minimum debt is 100, deposit more to increase your available mint amount: ${(maxMint??0).toFixed(2)}
           </Text>: null}
-          <QuickActionLTVWithSlider label="Your Debt" value={sliderValue}/>
         </Stack>
         {/* LTV Input Box */}
 

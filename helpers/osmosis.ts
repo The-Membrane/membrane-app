2e1de3570e68f23c4a327fdda44a0739d1aa99f7
Trix <triccs333@gmail>
2024-05-09 14:54:24 -0400

    move useasset up a layer

--- helpers/osmosis.ts
+++ helpers/osmosis.ts
@@ -24,7 +24,7 @@ export const swapToMsg = ({
     const microAmount = shiftDigits(cdtAmount, 6).dp(0).toString()
 
     //Swap to USDC
-    const cdtPrice = prices?.find((price) => price.denom === cdtAsset?.base)
+    const cdtPrice = prices?.find((price) => price.denom === cdtAsset.base)
     const swapToPrice = prices?.find((price) => price.denom === swapToAsset.base)
     const CDTInAmount = num(microAmount).div(2).toNumber()
 
@@ -33,6 +33,7 @@ export const swapToMsg = ({
 
 type GetLPMsgs = {
     cdtInAmount: string | number
+    cdtAsset: Asset
     pairedAssetInAmount:  string | number
     pairedAsset: Asset
     address: string
@@ -41,13 +42,12 @@ type GetLPMsgs = {
 export const LPMsg = ({
     address,
     cdtInAmount,
+    cdtAsset,
     pairedAssetInAmount,
     pairedAsset,
     poolID,
 }: GetLPMsgs) => {
-    const cdtAsset = useAssetBySymbol('CDT')
-
-    const CDTCoinIn = coin(cdtInAmount.toString(), cdtAsset?.base!)
-    const USDCCoinIn = coin(pairedAssetInAmount.toString(), pairedAsset?.base!)
+    const CDTCoinIn = coin(cdtInAmount.toString(), cdtAsset.base!)
+    const USDCCoinIn = coin(pairedAssetInAmount.toString(), pairedAsset.base!)
     return joinCLPools(address, CDTCoinIn, poolID, USDCCoinIn)
 }

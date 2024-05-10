c62e2424b72510236e0cff87037ecbb350ec6928
Trix <triccs333@gmail>
2024-05-10 14:30:32 -0400

    section logic into a new file to improve Home load time

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,98 +1,21 @@
-import { Card, HStack, Input, Stack, Text } from '@chakra-ui/react'
+import { Card, Stack, Text } from '@chakra-ui/react'
 import { StatsCard } from '../StatsCard'
 import ConfirmModal from '../ConfirmModal'
 import useCollateralAssets from '../Bid/hooks/useCollateralAssets'
 import useBalance from '@/hooks/useBalance'
-import QASelect from '@/components/QuickActionSelect'
-import useQuickActionState, { QuickActionState } from './hooks/useQuickActionState'
-import { ChangeEvent, useEffect, useState } from 'react'
+import useQuickActionState from './hooks/useQuickActionState'
+import { useEffect, useState } from 'react'
 import { num, shiftDigits } from '@/helpers/num'
-import { delayTime } from "@/config/defaults"
 import { Coin } from '@cosmjs/stargate'
 import { calcSliderValue } from '../Mint/TakeAction'
 import { useOraclePrice } from '@/hooks/useOracle'
-import { QuickActionAssetWithSlider } from './QuickActionAssetSlider'
-import { AssetWithBalance } from '../Mint/hooks/useCombinBalance'
 import { QuickActionLTVWithSlider } from './QuickActionLTVWithSlider'
 import useQuickActionVaultSummary from './hooks/useQuickActionVaultSummary'
 import useQuickAction from './hooks/useQuickAction'
 import { QASummary } from './QASummary'
 import useWallet from '@/hooks/useWallet'
 import { ConnectButton } from '../WallectConnect'
-
-type Props = {
-  value: string
-  onChange: (value: string) => void
-  assets: AssetWithBalance[]
-}
-
-const AssetsWithBalanceMenu = ({ value, onChange, assets }: Props) => {
-    return <QASelect options={assets} onChange={onChange} value={value} />
-}
-
-type SliderWithInputProps = {
-  max: number
-  inputBoxWidth?: string
-  QAState: QuickActionState
-  setQAState: (partialState: Partial<QuickActionState>) => void
-  onMenuChange: (value: string) => void
-  inputAmount: number
-  setInputAmount: (value: number) => void
-}
-
-const SliderWithInputBox = ({ max, inputBoxWidth = "38%", QAState, setQAState, onMenuChange, inputAmount, setInputAmount }: SliderWithInputProps) => {
-
-    const onSliderChange = (value: number) => {      
-      if (inputAmount != value) setInputAmount(value)
-    }
-
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault()
-      const newAmount = e.target.value
-      if (num(newAmount).isGreaterThan(max)) setInputAmount(max)
-        else setInputAmount(parseInt(e.target.value))
-
-      setTimeout(() => {
-        if (num(newAmount).isGreaterThan(max)) setQAState({ selectedAsset: { ...QAState?.selectedAsset, sliderValue: max }})
-          else setQAState({ selectedAsset: { ...QAState?.selectedAsset, sliderValue: (parseInt(e.target.value)) }})
-      }, delayTime);  
-    }
-
-    useEffect(() => {
-      //If the selected asset has a different slider value than the inputAmount, set the inputAmount to the slider value
-      if (QAState?.selectedAsset?.sliderValue != inputAmount) {
-        setInputAmount(QAState?.selectedAsset?.sliderValue??0)
-      }
-    }, [QAState?.selectedAsset?.sliderValue])
-    
-
-
-    return (
-    <Stack py="5" w="full" gap="5">     
-      <Text fontSize="14px" fontWeight="700">
-        Choose Collateral
-      </Text> 
-      {QAState?.selectedAsset != undefined ? <><HStack justifyContent="space-between">
-        <AssetsWithBalanceMenu 
-          value={QAState?.selectedAsset} 
-          onChange={onMenuChange}
-          assets={QAState?.assets}
-        />
-        <Input 
-          width={inputBoxWidth} 
-          textAlign={"center"} 
-          placeholder="0" 
-          type="number" 
-          value={inputAmount} 
-          onChange={handleInputChange}
-        />
-      </HStack>
-      <QuickActionAssetWithSlider onChangeExt={onSliderChange} key={QAState?.selectedAsset?.base} asset={QAState?.selectedAsset} label={QAState?.selectedAsset?.symbol} />
-      </> : null}
-
-  </Stack>)
-}
-
+import { SliderWithInputBox } from './QuickActionSliderInput'
 
 const Home = () => {
   const { isWalletConnected } = useWallet()
@@ -174,6 +97,9 @@ const Home = () => {
 
   return (
     <Stack >
+      <Text variant="title" fontSize="16px">
+        $CDT: The safety net for Interchain risk takers
+      </Text>
       <StatsCard />      
       <Card w="384px" alignItems="center" justifyContent="space-between" p="8" gap="0">
         <Text variant="title" fontSize="16px">

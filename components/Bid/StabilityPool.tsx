a9dd8b8485157002407c7fa892aa017c0315710b
Trix <triccs333@gmail>
2024-05-20 13:28:19 -0400

    logs for SP unstake

--- components/Bid/StabilityPool.tsx
+++ components/Bid/StabilityPool.tsx
@@ -20,9 +20,10 @@ import { ChangeEvent, useState } from 'react'
 import useStabilityAssetPool from './hooks/useStabilityAssetPool'
 import useWithdrawStabilityPool from './hooks/useWithdrawStabilityPool'
 
-const UnstakeButton = ({ amount }: { amount: string }) => {
+const UnstakeButton = ({ amount }: { amount: string }) => {]
+  console.log("unstake", amount)
   const withdraw = useWithdrawStabilityPool(amount)
-  console.log({ withdraw })
+  
   return (
     <TxButton
       w="150px"

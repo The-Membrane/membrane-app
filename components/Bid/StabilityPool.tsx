fd1e17a1ee10efc064ede5c7e1989a3c5fc4eacb
Trix <triccs333@gmail>
2024-05-20 13:54:29 -0400

    test conditional for unstaking SP bids

--- components/Bid/StabilityPool.tsx
+++ components/Bid/StabilityPool.tsx
@@ -108,7 +108,7 @@ const DepositAsset = ({ deposit, index }: { deposit: Deposit; index: number }) =
     setInputAmount(amount)
   }
   console.log(isEnded, deposit.unstake_time)
-  if (!isEnded && deposit.unstake_time) {
+  if (deposit.unstake_time && getSPTimeLeft(deposit.unstake_time).minutesLeft > 0) {
     return (
       <HStack alignItems="flex-start" gap="5">
         <Box bg="blackAlpha.500" borderRadius="md" px="4" py="1" h="full">

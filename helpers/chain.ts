cff78b32e1343ba2c62fc9b33f7451b655d6375f
Trix <triccs333@gmail>
2024-05-31 15:37:52 -0400

    asset logs

--- helpers/chain.ts
+++ helpers/chain.ts
@@ -41,7 +41,7 @@ export const getChainAssets = (chainID: string = 'osmosis') => {
 }
 export const getAssets = (chainID: string = 'osmosis') => {
   const chainAssets = registryAssets.find((asset) => asset.chain_name === chainID)
-  console.log(chainID, chainAssets)
+  console.log(chainID, registryAssets)
   const supportedChainAssets = chainAssets?.assets.filter((asset) =>
     supportedAssets.includes(asset.symbol),
   )

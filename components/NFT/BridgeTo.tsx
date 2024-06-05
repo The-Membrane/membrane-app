f7aa576e54e7650f7664ee2704e31ec21b49f883
Trix <triccs333@gmail>
2024-06-05 13:38:21 -0400

    conditional chain name for button label

--- components/NFT/BridgeTo.tsx
+++ components/NFT/BridgeTo.tsx
@@ -6,11 +6,12 @@ import useNFTState from "./hooks/useNFTState"
 import { isGreaterThanZero } from "@/helpers/num"
 import { TxButton } from "../TxButton"
 import useIBC from "./hooks/useIBC"
-import { useState } from "react"
 import QuickActionWidget from "../Home/QuickActionWidget"
+import useQuickActionState from "../Home/hooks/useQuickActionState"
 
-const BridgeToStargaze = () => {
+const BridgeTo = () => {
     const { NFTState, setNFTState } = useNFTState()
+    const { quickActionState } = useQuickActionState()
     const ibc = useIBC()
 
     const mbrn = useAssetBySymbol('MBRN')
@@ -26,11 +27,9 @@ const BridgeToStargaze = () => {
         setNFTState({ mbrnBridgeAmount: value })
     }
 
-    const [swapToggle, setSwapTopggle] = useState(false)
-
     return (
         <Stack w="full" gap="5">
-        <Text variant="title">Bridge From Osmosis To Stargaze</Text>
+        <Text variant="title">Bridge</Text>
         <QuickActionWidget actionMenuOptions={[{ value: "Bridge to Stargaze", label: "Bridge to Stargaze" }, { value: "Bridge to Osmosis", label: "Bridge to Osmosis"}]} bridgeCardToggle={true}/>
         {/**/}
         <Card w="full" p="8" marginTop={"5.1%"} alignItems="center" gap={5} h="28%" justifyContent="space-between">            
@@ -70,9 +69,9 @@ const BridgeToStargaze = () => {
                     isDisabled={(!isGreaterThanZero(NFTState.mbrnBridgeAmount) && !isGreaterThanZero(NFTState.cdtBridgeAmount)) || ibc?.simulate.isError || !ibc?.simulate.data}
                     isLoading={ibc.simulate.isPending && !ibc.simulate.isError && ibc.simulate.data}
                     onClick={() => ibc.tx.mutate()}
-                    chain_name="stargaze"
+                    chain_name={quickActionState.action.value === "Bridge to Stargaze" ? "stargaze" : "osmosis"}
                     >
-                    Bridge to Stargaze
+                    {quickActionState.action.value}
                 </TxButton>
             </Stack>
         </Card>
@@ -80,4 +79,4 @@ const BridgeToStargaze = () => {
     )
 }
 
-export default BridgeToStargaze
+export default BridgeTo

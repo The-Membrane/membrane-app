ee331df3f1153fd8843c8c00cb64abeaece6844a
Trix <triccs333@gmail>
2024-06-06 12:41:04 -0400

    format the bridge card

--- components/NFT/BridgeTo.tsx
+++ components/NFT/BridgeTo.tsx
@@ -1,17 +1,8 @@
-import { Card, HStack, Text, Stack, Checkbox } from "@chakra-ui/react"
-import { SliderWithState } from "../Mint/SliderWithState"
-import { useAssetBySymbol } from "@/hooks/useAssets"
-import { useBalanceByAsset } from "@/hooks/useBalance"
-import useNFTState from "./hooks/useNFTState"
-import { isGreaterThanZero } from "@/helpers/num"
-import { TxButton } from "../TxButton"
+import { Text, Stack } from "@chakra-ui/react"
 import useIBC from "./hooks/useIBC"
 import QuickActionWidget from "../Home/QuickActionWidget"
-import useQuickActionState from "../Home/hooks/useQuickActionState"
 
 const BridgeTo = () => {
-    const { NFTState, setNFTState } = useNFTState()
-    const { quickActionState } = useQuickActionState()
     const ibc = useIBC()
 
     // const mbrn = useAssetBySymbol('MBRN')
@@ -29,54 +20,9 @@ const BridgeTo = () => {
 
     return (
         <Stack w="full" gap="5">
-        <Text variant="title">Bridge</Text>
-        <QuickActionWidget actionMenuOptions={[{ value: "Bridge to Stargaze", label: "Bridge to Stargaze" }, { value: "Bridge to Osmosis", label: "Bridge to Osmosis"}]} bridgeCardToggle={true}/>
+            <Text variant="title">Bridge</Text>
+            <QuickActionWidget actionMenuOptions={[{ value: "Bridge to Stargaze", label: "Bridge to Stargaze" }, { value: "Bridge to Osmosis", label: "Bridge to Osmosis"}]} bridgeCardToggle={true}/>
         </Stack>
-        {/**/}
-        {/* <Card w="full" p="8" marginTop={"5.1%"} alignItems="center" gap={5} h="28%" justifyContent="space-between">             */}
-            {/* <Stack w="full" gap="1">
-                <HStack justifyContent="space-between">
-                    <Text fontSize="16px" fontWeight="700">
-                    CDT
-                    </Text>
-                    <Text fontSize="16px" fontWeight="700">
-                    {NFTState.cdtBridgeAmount}
-                    </Text>
-                </HStack>
-                <SliderWithState
-                    value={NFTState.cdtBridgeAmount}
-                    onChange={onCDTChange}
-                    min={0}
-                    max={Number(osmosisCDTBalance)}
-                />
-                <HStack justifyContent="space-between">
-                    <Text fontSize="16px" fontWeight="700">
-                    MBRN
-                    </Text>
-                    <Text fontSize="16px" fontWeight="700">
-                    {NFTState.mbrnBridgeAmount}
-                    </Text>
-                </HStack>
-                <SliderWithState
-                    value={NFTState.mbrnBridgeAmount}
-                    onChange={onMBRNChange}
-                    min={0}
-                    max={Number(osmosisMBRNBalance)}
-                /> */}
-                {/* <TxButton
-                    marginTop={"3%"}
-                    w="100%"
-                    px="10"
-                    isDisabled={(!isGreaterThanZero(NFTState.mbrnBridgeAmount) && !isGreaterThanZero(NFTState.cdtBridgeAmount)) || ibc?.simulate.isError || !ibc?.simulate.data}
-                    isLoading={ibc.simulate.isPending && !ibc.simulate.isError && ibc.simulate.data}
-                    onClick={() => ibc.tx.mutate()}
-                    chain_name={quickActionState.action.value === "Bridge to Stargaze" ? "stargaze" : "osmosis"}
-                    >
-                    {quickActionState.action.value}
-                </TxButton> */}
-            {/* </Stack> */}
-        {/* </Card> */}
-        
     )
 }
 

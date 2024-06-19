2be2a8d010d61f56e456df347d35c5af0fb239a9
Trix <triccs333@gmail>
2024-06-19 09:09:55 -0400

    fix auction claim button

--- components/Stake/AuctionClaim.tsx
+++ components/Stake/AuctionClaim.tsx
@@ -30,24 +30,25 @@ const Stake = React.memo(() => {
   
   console.log(!isGreaterThanZero(MBRNBalance), claim?.simulate.errorMessage, claim?.simulate.isError, !claim?.simulate.data)
   return (
-    <ConfirmModal 
-        action={claim}
-        label={"Begin Degeneracy"}
-        isDisabled={!isGreaterThanZero(MBRNBalance) }>
-          {/* <QASummary newPositionValue={parseInt(newPositionValue.toFixed(0))} swapRatio={swapRatio} summary={summary}/> */}
-        </ConfirmModal>
-    //   <TxButton
-    //     // marginTop={"3%"}
-    //     w="full"
-    //     height="64px"
-    //     px="10"
-    //     isDisabled={!isGreaterThanZero(MBRNBalance) }//|| claim?.simulate.isError || !claim?.simulate.data}
-    //     isLoading={claim.simulate.isPending && !claim.simulate.isError && claim.simulate.data}
-    //     onClick={() => claim.tx.mutate()}
-    //     toggleConnectLabel={false}
-    //     >
-    //     {discount}% Discount on Fee Auction
-    // </TxButton>
+    // <ConfirmModal 
+    //     action={claim}
+    //     label={"Begin Degeneracy"}
+    //     isDisabled={!isGreaterThanZero(MBRNBalance) }>
+          
+    //       {/* <QASummary newPositionValue={parseInt(newPositionValue.toFixed(0))} swapRatio={swapRatio} summary={summary}/> */}
+    //     </ConfirmModal>
+      <TxButton
+        // marginTop={"3%"}
+        w="full"
+        height="64px"
+        px="10"
+        isDisabled={!isGreaterThanZero(MBRNBalance) || claim?.simulate.isError || !claim?.simulate.data}
+        isLoading={claim.simulate.isPending && !claim.simulate.isError && claim.simulate.data}
+        onClick={() => claim.tx.mutate()}
+        toggleConnectLabel={false}
+        >
+        {discount}% Discount on Fee Auction
+    </TxButton>
   )
 })
 

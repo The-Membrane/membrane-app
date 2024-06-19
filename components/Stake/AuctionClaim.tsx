4d113c3db9931f326b402275ac455f4d64b28f05
Trix <triccs333@gmail>
2024-06-18 21:14:49 -0400

    new query keys for sim, restyle, fix discount

--- components/Stake/AuctionClaim.tsx
+++ components/Stake/AuctionClaim.tsx
@@ -24,7 +24,7 @@ const Stake = React.memo(() => {
     const timeElapsed = startTime.diff(currentTime, 'second')
     const discount = parseInt((timeElapsed / auctionDiscountIncreaseTimeframe).toFixed(0)) * auctionDiscount
 
-    return Math.max(discount, 1)
+    return Math.max(discount, 0.99) * 100
   }, [feeAuctions])
   
   return (
@@ -32,7 +32,6 @@ const Stake = React.memo(() => {
         // marginTop={"3%"}
         w="full"
         height="64px"
-        borderRadius="50%"
         px="10"
         isDisabled={!isGreaterThanZero(MBRNBalance) || claim?.simulate.isError || !claim?.simulate.data}
         isLoading={claim.simulate.isPending && !claim.simulate.isError && claim.simulate.data}

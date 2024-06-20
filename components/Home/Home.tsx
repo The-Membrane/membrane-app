39d4c6d7131def4516e9aef55fffbdb752a6c76d
Trix <triccs333@gmail>
2024-06-20 14:02:43 -0400

    remove cookie usage

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,13 +1,10 @@
 import { Stack } from '@chakra-ui/react'
 import { StatsCard } from '../StatsCard'
 import QuickActionWidget from './QuickActionWidget'
-import { getCookie, setCookie } from '@/helpers/cookies'
 
 import React from "react"
 
 const Home = React.memo(() => {
-  var cook = getCookie('test')
-  console.log("finsihed cooking", cook)
   return (
     <Stack >
       <StatsCard />

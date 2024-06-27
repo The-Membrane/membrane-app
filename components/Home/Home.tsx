0dc2c53d9e72f6e16a1db4277f549a4bfc578c85
Trix <triccs333@gmail>
2024-06-26 20:53:34 -0400

    test dynamic grid size on home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -18,16 +18,13 @@ const Home = React.memo(() => {
       templateColumns={{base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)',}}
       gap={4}
     >
-        <GridItem colSpan={1} h='10'> 
+        <GridItem colSpan={{base: "auto", md: 1}} rowSpan={{base: 1, md: "auto"}} h='10'> 
         <Stack >
           {range(0, MAX_CDP_POSITIONS).map((index) => <PerformanceStats key={index} positionIndex={index} />)}          
         </Stack>
         </GridItem>
-        <GridItem colSpan={1} h='10' > 
+        <GridItem colSpan={{base: "auto", md: 1}} rowSpan={{base: 1, md: "auto"}} h='10' > 
           <QuickActionWidget />
-        </GridItem>
-        <GridItem colSpan={1} h='10'> 
-          {/* scatter collateral icons & hover to show symbol name */}
         </GridItem>
         </Grid>
     </Stack>

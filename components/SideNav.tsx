6f17fd6f34d1cb8cf4a093cff815ab1bf1a270fa
Trix <triccs333@gmail>
2024-06-26 21:25:08 -0400

    test mobile copy paste

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,7 +1,17 @@
-import { Box, Button, HStack, Stack, Text, Image } from '@chakra-ui/react'
+import { Box, HStack, Stack, Text, Image, IconButton, 
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  VStack,
+  Link as ChakraLink,
+  Button, 
+  Flex} from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import { BidIcon, ClaimIcon, HomeIcon, MintIcon, StakeIcon, NFTAuctionIcon } from './Icons'
 import Logo from './Logo'
 import WallectConnect from './WallectConnect'
@@ -9,12 +19,12 @@ import { BalanceCard } from './BalanceCard'
 import useProtocolClaims from './Nav/hooks/useClaims'
 import ConfirmModal from './ConfirmModal'
 import { ClaimSummary } from './Bid/ClaimSummary'
-import { num } from '@/helpers/num'
 import useProtocolLiquidations from './Nav/hooks/useLiquidations'
 import { LiqSummary } from './Nav/LiqSummary'
-import { getPriceByDenom } from '@/services/oracle'
 import { getAssetBySymbol } from '@/helpers/chain'
 import { useOraclePrice } from '@/hooks/useOracle'
+
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 type NavItems = {
   label: string
@@ -71,6 +81,15 @@ const getCDTPrice = () => {
 }
 
 const SideNav = React.memo(() => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuToggle = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const close = () => {
+    setMobileMenuOpen(false);
+  };
   const [cdtPrice, setcdtPrice ] = useState(" ")
   const price = getCDTPrice()
   if (price != cdtPrice && price != '0') setcdtPrice(price)
@@ -83,7 +102,23 @@ const SideNav = React.memo(() => {
   setTimeout(() => setEnableMsgs(true), 2222);
   
   return (
-    <Stack as="aside" w={[0, 'full']} maxW="256px" minW="200px" h="100%" p="6" bg="whiteAlpha.100" style={{zoom: '85%'}}>
+    <Flex
+      px={4}
+      py={2}
+      bg="rgba(0, 0, 0, 0.15)"
+      color="white"
+      width="100vw"
+      zIndex={1}
+      justifyContent="center"
+    >
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        width="100vw"
+        maxW="7xl"
+      >
+    <Stack as="aside" w={[0, 'full']} maxW="256px" minW="200px" h="100%" p="6" bg="whiteAlpha.100" style={{zoom: '85%'}} display={{ base: "none", md: "flex" }}>
       <Stack as="ul" gap="1">
         <Stack marginTop={"6%"}>
           <Logo />
@@ -119,6 +154,60 @@ const SideNav = React.memo(() => {
 
       <BalanceCard />
     </Stack>
+
+     {/* Mobile Menu */}
+     <HStack display={{ base: "flex", md: "none" }} spacing="0.5rem">
+     {!isMobileMenuOpen && (
+       <IconButton
+         icon={<HamburgerIcon />}
+         aria-label={"Open Menu"}
+         bg="transparent"
+         color="white"
+         onClick={handleMobileMenuToggle}
+       />
+     )}
+   </HStack>
+ </Flex>
+
+ {/* Mobile Menu Modal */}
+ <Modal isOpen={isMobileMenuOpen} onClose={close}>
+   <ModalOverlay bg="rgba(0, 0, 0, 0.7)" />
+   <ModalContent bg="transparent" color="white" mt={4}>
+     <ModalCloseButton mr={2} />
+     <ModalBody mt={8}>
+       <VStack spacing={8} mt={12}>
+         <ChakraLink fontWeight="medium" href="#home" onClick={close}>
+           Home
+         </ChakraLink>
+         <ChakraLink fontWeight="medium" href="#protocol" onClick={close}>
+           Protocol Overview
+         </ChakraLink>
+         <ChakraLink
+           fontWeight="medium"
+           href="#governance"
+           onClick={close}
+         >
+           Governance
+         </ChakraLink>
+         <ChakraLink fontWeight="medium" href="#vision" onClick={close}>
+           The Vision
+         </ChakraLink>
+         <ChakraLink fontWeight="medium" href="#faqs" onClick={close}>
+           FAQs
+         </ChakraLink>
+         <ChakraLink
+           href="https://membrane-ui-mainnet.vercel.app/"
+           target="_blank"
+         >
+           <Button borderRadius="full" bg="#798eff">
+             Launch App
+           </Button>
+         </ChakraLink>
+       </VStack>
+     </ModalBody>
+   </ModalContent>
+ </Modal>
+ </Flex>
   )
 })
 

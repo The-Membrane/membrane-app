af8778834fe4667364dcbfe6aa50bac502926eeb
Trix <triccs333@gmail>
2024-06-28 10:54:47 -0400

    format mobile home & remove stats if positions haven't loaded yet

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -6,8 +6,10 @@ import React from "react"
 import { range } from 'lodash'
 import { MAX_CDP_POSITIONS } from '@/config/defaults'
 import PerformanceStats from './PerformanceStats'
+import { useUserPositions } from '@/hooks/useCDP'
 
 const Home = React.memo(() => {
+  const { data: basketPositions } = useUserPositions()
   return (
     <Stack >
       <StatsCard />
@@ -16,10 +18,10 @@ const Home = React.memo(() => {
       //Mobile has 3 rows and 1 column, Desktop has 1 row and 3 columns
       templateRows={{base: 'repeat(3, 1fr)', md: 'repeat(1, 1fr)'}} 
       templateColumns={{base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)'}}
-      gap={56}
+      gap={{base: 56, md: 4}}
     >
         <GridItem colSpan={{base: "auto", md: 1}} rowSpan={{base: 1, md: "auto"}} h='10'> 
-        <Stack flexWrap="wrap" alignContent="center">
+        <Stack flexWrap="wrap" alignContent="center" styles={!basketPositions ?{ display: "none"} : null }>
           {range(0, MAX_CDP_POSITIONS).map((index) => <PerformanceStats key={index} positionIndex={index} />)}          
         </Stack>
         </GridItem>

156ebd74bd941d9266c5d053e03045560d7d423d
Trix <triccs333@gmail>
2024-07-02 14:47:52 -0400

    test mobile Stake layout

--- components/Stake/Stake.tsx
+++ components/Stake/Stake.tsx
@@ -1,21 +1,26 @@
 import Governance from '@/components/Governance'
-import { HStack, Stack, Text } from '@chakra-ui/react'
+import { HStack, Stack, Text, useBreakpointValue } from '@chakra-ui/react'
 import ManageStake from './ManageStake'
 import Delegate from '@/components/Governance/Delegate'
 import React from "react"
 import AuctionClaim from './AuctionClaim'
 
-const Stake = React.memo(() => {
+const Stake = React.memo(() => {  
+  const isMobile = useBreakpointValue({ base: true, md: false })
+  
   return (
-    <HStack gap="5" w="full" alignItems="flex-start">
+    <Stack direction={{base: "column", md: "row"}} gap="5" w="full" alignItems="flex-start">
       <Stack w="full" gap="5">
         <Text variant="title">Governance</Text>
         <ManageStake />
+        {!isMobile ? <>
         <Delegate />
         <AuctionClaim />
+        </>
+      : null}
       </Stack>
       <Governance />
-    </HStack>
+    </Stack>
   )
 })
 

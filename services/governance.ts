2425259685af4709601506275b0289395255ac76
Trix <triccs333@gmail>
2024-07-05 14:51:25 -0400

    remove dele vote calc

--- services/governance.ts
+++ services/governance.ts
@@ -193,12 +193,12 @@ export const getProposals = async () => {
       }),
     )
 
-    console.log(allProposals)
-  //Count the amount of times a delegate has voted for a proposal & save in an array
-  const delegateVotes = delegates.map((delegate) => {
-    return [delegate.name, allProposals.filter((proposal) => proposal.for_voters?.includes(delegate.address) || proposal.against_voters?.includes(delegate.address) || proposal.aligned_voters?.includes(delegate.address) || proposal.removal_voters?.includes(delegate.address)).length]
-  })
-  console.log(delegateVotes)
+  //   console.log(allProposals)
+  // //Count the amount of times a delegate has voted for a proposal & save in an array
+  // const delegateVotes = delegates.map((delegate) => {
+  //   return [delegate.name, allProposals.filter((proposal) => proposal.for_voters?.includes(delegate.address) || proposal.against_voters?.includes(delegate.address) || proposal.aligned_voters?.includes(delegate.address) || proposal.removal_voters?.includes(delegate.address)).length]
+  // })
+  // console.log(delegateVotes)
 
 
   return allProposals.filter((prop) => prop.proposal_id != "61")

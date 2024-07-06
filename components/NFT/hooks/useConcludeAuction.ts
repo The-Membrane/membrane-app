4d531a9c288a91b0717fa32d573e28a055a8cef7
Trix <triccs333@gmail>
2024-07-06 11:29:59 -0400

    submit next 3

--- components/NFT/hooks/useConcludeAuction.ts
+++ components/NFT/hooks/useConcludeAuction.ts
@@ -17,18 +17,18 @@ const useConcludeAuction = () => {
       const messageComposer = new BraneAuctionMsgComposer(address, contracts.brane_auction)
 
       //Conclude Msg
-      const submitNFT = messageComposer.submitNft({
-        proceedRecipient: "stars18a9canxqrs9afxj68gcwvlkq2vzm58vjcuaarc",
-        tokenUri: "ipfs://bafybeib64m6rdz7ukydoze6qzneq76xlznalyfyw2w7d43brzuhekel4bu/",
-      })
-      const submitNFT1 = messageComposer.submitNft({
-        proceedRecipient: "stars1pyxg2vgej0e3dmpg44kz2us0efyctq94v6t2h7",
-        tokenUri: "ipfs://bafybeib4imygu5ehbgy7frry65ywpekw72kbs7thk5a2zjhyw67wluoy2m/metadata/Barbrane",
-      })
-      const submitNFT2 = messageComposer.submitNft({
-        proceedRecipient: "stars1pyxg2vgej0e3dmpg44kz2us0efyctq94v6t2h7",
-        tokenUri: "ipfs://bafybeib4imygu5ehbgy7frry65ywpekw72kbs7thk5a2zjhyw67wluoy2m/metadata/Branda",
-      })
+      // const submitNFT = messageComposer.submitNft({
+      //   proceedRecipient: "stars18a9canxqrs9afxj68gcwvlkq2vzm58vjcuaarc",
+      //   tokenUri: "ipfs://bafybeib64m6rdz7ukydoze6qzneq76xlznalyfyw2w7d43brzuhekel4bu/",
+      // })
+      // const submitNFT1 = messageComposer.submitNft({
+      //   proceedRecipient: "stars1pyxg2vgej0e3dmpg44kz2us0efyctq94v6t2h7",
+      //   tokenUri: "ipfs://bafybeib4imygu5ehbgy7frry65ywpekw72kbs7thk5a2zjhyw67wluoy2m/metadata/Barbrane",
+      // })
+      // const submitNFT2 = messageComposer.submitNft({
+      //   proceedRecipient: "stars1pyxg2vgej0e3dmpg44kz2us0efyctq94v6t2h7",
+      //   tokenUri: "ipfs://bafybeib4imygu5ehbgy7frry65ywpekw72kbs7thk5a2zjhyw67wluoy2m/metadata/Branda",
+      // })
       const submitNFT3 = messageComposer.submitNft({
         proceedRecipient: "stars1pyxg2vgej0e3dmpg44kz2us0efyctq94v6t2h7",
         tokenUri: "ipfs://bafybeib4imygu5ehbgy7frry65ywpekw72kbs7thk5a2zjhyw67wluoy2m/metadata/Cheeky Brane",
@@ -52,7 +52,7 @@ const useConcludeAuction = () => {
       // const curate = messageComposer.voteToCurate({submissionIds: [12, 13, 14,15,16,17,18,19]})
       // const msg = messageComposer.concludeAuction()
 
-      return [submitNFT, submitNFT1, submitNFT2] as MsgExecuteContractEncodeObject[]
+      return [submitNFT3, submitNFT4, submitNFT5] as MsgExecuteContractEncodeObject[]
     },
     enabled: !!address,
   })

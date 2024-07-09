db41d7d52ee06425c7bc9ef97ad5b23976533b51
Trix <triccs333@gmail>
2024-07-09 17:19:12 -0400

    test slider assignment for multiple assets

--- components/Home/QuickActionAssetSlider.tsx
+++ components/Home/QuickActionAssetSlider.tsx
@@ -25,7 +25,11 @@ export const QuickActionAssetWithSlider = ({ asset, label, onChangeExt }: AssetW
       //Find the asset in quickActionState levAssets and update the sliderValue
       let found = quickActionState?.levAssets?.findIndex((levAsset) => asset.symbol === levAsset.symbol)
       // console.log(quickActionState.levAssets?.find((levAsset) => asset.symbol === levAsset.symbol), quickActionState.levAssets?.find((levAsset) => asset.symbol === levAsset.symbol) != undefined, asset.symbol, quickActionState.levAssets)
-      if((found??0) > 0 && quickActionState.levAssets) quickActionState.levAssets[found??0] = asset
+      if((found??0) > 0 && quickActionState.levAssets) quickActionState.levAssets[found??0] = {
+        ...asset,
+        amount,
+        sliderValue,
+      }
       // && quickActionState.levAssets?.find((levAsset) => asset.symbol === levAsset.symbol) != undefined
 
       return {

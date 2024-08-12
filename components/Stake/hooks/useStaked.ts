46f43fcd63f8b872e078c32366169591cc85f62f
Trix <triccs333@gmail>
2024-08-11 20:53:57 -0400

    logs

--- components/Stake/hooks/useStaked.ts
+++ components/Stake/hooks/useStaked.ts
@@ -1,6 +1,7 @@
 import useWallet from '@/hooks/useWallet'
 import { getRewards, getStaked } from '@/services/staking'
 import { useQuery } from '@tanstack/react-query'
+import { cond } from 'lodash'
 
 const useStaked = () => {
   const { address } = useWallet()
@@ -11,6 +12,7 @@ const useStaked = () => {
       if (!address) return null
       let static_address = "osmo1pss5jer8r00zv6x562uttctug9u96f4k2g0kam"
       const { staked, unstaking } = await getStaked(static_address)
+      console.log("unstaked", unstaking)
       const rewards = await getRewards(static_address) || []
 
       //Reward query is erroring

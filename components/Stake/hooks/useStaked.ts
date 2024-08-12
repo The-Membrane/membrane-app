bdcc1145c56fe3a73a4d7dcd6e84b47bd5668325
Trix <triccs333@gmail>
2024-08-11 20:22:38 -0400

    logs

--- components/Stake/hooks/useStaked.ts
+++ components/Stake/hooks/useStaked.ts
@@ -8,9 +8,10 @@ const useStaked = () => {
   return useQuery({
     queryKey: ['staked', address],
     queryFn: async () => {
+      console.log("address check", address)
       if (!address) return null
       let static_address = "osmo1pss5jer8r00zv6x562uttctug9u96f4k2g0kam"
-
+      console.log("got past address check")
       const { staked, unstaking } = await getStaked(static_address)
       const rewards = await getRewards(static_address)
 

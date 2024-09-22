b47efb76e682a25e0d6bb3484231b4f2a1198e16
Trix <triccs333@gmail>
2024-09-22 18:34:45 -0400

    balance check

--- components/Earn/Deposit.tsx
+++ components/Earn/Deposit.tsx
@@ -121,8 +121,8 @@ const Deposit = () => {
   
   const loopedUSDCAsset = useAssetBySymbol('loopedUSDCmars')
   const loopedUSDCBalance = useBalanceByAsset(loopedUSDCAsset)
-  const { data: underlyingUSDC } = useVaultTokenUnderlying(loopedUSDCBalance)
-  console.log("underlyingUSDC", loopedUSDCAsset, loopedUSDCBalance, underlyingUSDC)
+  const { data: underlyingUSDC } = useVaultTokenUnderlying(shiftDigits(loopedUSDCBalance, -6).toString())
+  console.log("underlyingUSDC", loopedUSDCAsset, loopedUSDCBalance, shiftDigits(loopedUSDCBalance, -6), underlyingUSDC)
   
   const { data: APRs } = useAPR() 
   const APRObject = useMemo(() => {

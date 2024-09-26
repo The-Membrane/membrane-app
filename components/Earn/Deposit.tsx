8b5f075aaa7933b3cefb1423d663c6bf119dff9a
Trix <triccs333@gmail>
2024-09-25 20:09:27 -0400

    descrp change

--- components/Earn/Deposit.tsx
+++ components/Earn/Deposit.tsx
@@ -182,14 +182,14 @@ const Deposit = () => {
           
           <Card>
               <Text variant="title" fontSize={"md"} letterSpacing={"1px"}>Who is the Yield?</Text>
-              <Text variant="body" fontWeight={"bold"}> TLDR: Looped Mars USDC yield, CDT Redemptions & a 0.5% Exit fee. {'\n'}{'\n'}</Text>
+              <Text variant="body" fontWeight={"bold"}> TLDR: Looped Mars USDC yield, CDT Redemptions & a 0.5% entry fee {'\n'}{'\n'}</Text>
               <Text variant="body">
                 This vault supplies USDC on Mars Protocol and loops it by collateralizing the Mars position to mint CDT,
                 swap it for USDC & deposit it back to the Mars USDC market. The vault's collateral position is also open for profitable debt redemptions that act as downside liquidity for CDT which adds additional yield to depositors while keeping CDT's peg tight.
-                On top of that, there is a 0.5% exit fee that goes to remaining depositors in order to account for the slippage it takes to unloop & withdraw USDC.
-                The exit fee from withdrawals that use the buffer of supplied USDC are pure profit for depositors, whereas withdrawals that need to be swapped will only be profitable if the slippage is lower than the max allowed slippage of 0.5%.
+                On top of that, there is a 0.5% entry fee in order to account for the slippage it takes to unloop & withdraw USDC.
+                The entry fee from withdrawals that use the buffer of supplied USDC are pure profit for depositors, whereas withdrawals that need to be swapped will only be profitable if the slippage is lower than the max allowed slippage of 0.5%. You only see the profits for the entry fee as people withdraw, beforehand they still have virtual ownership over the full deposit.
               </Text>          
-              <Text variant="title" fontSize={"md"} letterSpacing={"1px"}>{'\n'}{'\n'}Recommended Deposit Time: ~{num(EXIT_FEE).dividedBy(num(APRs?.month_apr??"0").dividedBy(365)).toFixed(1)} days to overcome exit fee</Text>
+              <Text variant="title" fontSize={"md"} letterSpacing={"1px"}>{'\n'}{'\n'}Recommended Deposit Time: ~{num(EXIT_FEE).dividedBy(num(APRs?.month_apr??"0").dividedBy(365)).toFixed(1)} days to overcome entry fee</Text>
             </Card>
             {/* <Card>
               <Text variant="title" fontSize={"md"} letterSpacing={"1px"}>Global Vault Info</Text>

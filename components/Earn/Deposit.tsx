1397ccc34a7e72025a8bb0683883d959ede269a5
Trix <triccs333@gmail>
2024-10-03 12:08:52 -0400

    test global vault info

--- components/Earn/Deposit.tsx
+++ components/Earn/Deposit.tsx
@@ -176,9 +176,9 @@ const Deposit = () => {
             <Card>
               <Text variant="title" fontSize={"md"} letterSpacing={"1px"}>Global Vault Info</Text>
               <HStack justifyContent="end" width={"100%"} gap={"1rem"}>
-                <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >TVL: </Text><Text variant="body">${(vaultInfo?.totalTVL??0).toFixed(0)}</Text></HStack>
-                <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >Debt (redeemable): </Text><Text variant="body">{vaultInfo?.debtAmount??0} CDT</Text></HStack>
-                <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >Leverage: </Text><Text variant="body">{vaultInfo?.leverage??"N/A"}x</Text></HStack>
+                <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >TVL: </Text><Text variant="body">${vaultInfo?.totalTVL.toFixed(0)}</Text></HStack>
+                <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >Debt (redeemable): </Text><Text variant="body">{vaultInfo?.debtAmount} CDT</Text></HStack>
+                <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >Leverage: </Text><Text variant="body">{vaultInfo?.leverage.toString()}x</Text></HStack>
               </HStack>
           </Card>
           <Card p="8" gap={5} width={"100%"}>
--- components/Earn/hooks/useEarnQueries.ts
+++ components/Earn/hooks/useEarnQueries.ts
@@ -105,7 +105,7 @@ export const useVaultInfo = () => {
                 debtValue,
                 leverage,
                 cost,
-                debtAmount: shiftDigits(vaultCDP?.credit_amount, -6).toFixed(0)
+                debtAmount: parseInt(shiftDigits(vaultCDP?.credit_amount, -6))
             }
         },
     })

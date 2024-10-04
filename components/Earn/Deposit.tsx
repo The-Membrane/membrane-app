5a800905149f88b6420d6e677a7f74ceb0d81e0b
Trix <triccs333@gmail>
2024-10-04 16:33:50 -0400

    format

--- components/Earn/Deposit.tsx
+++ components/Earn/Deposit.tsx
@@ -183,7 +183,7 @@ const Deposit = () => {
                 <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >Debt: </Text><Text variant="body">{vaultInfo.debtAmount} CDT</Text></HStack>
                 <HStack><Text variant="title" fontSize={"md"} letterSpacing={"1px"} >Leverage: </Text><Text variant="body">{vaultInfo.leverage.toFixed(2)}x</Text></HStack></>
                 : 
-                <Text variant="body">Loading...</Text>}
+                <Text variant="body" justifyContent="center">loading...</Text>}
               </HStack>
           </Card>
           <Card p="8" gap={5} width={"100%"}>

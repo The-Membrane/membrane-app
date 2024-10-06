337ca9390545b250ae6d4ace565f20ae3390f124
Trix <triccs333@gmail>
2024-10-06 15:28:26 -0400

    test new redeem withdraw logic + logs

--- components/Earn/hooks/useCDPRedeem.ts
+++ components/Earn/hooks/useCDPRedeem.ts
@@ -14,10 +14,16 @@ import { denoms } from '@/config/defaults'
 import useBalance from '@/hooks/useBalance'
 import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
 import { toUtf8 } from "@cosmjs/encoding";
+import { useOraclePrice } from '@/hooks/useOracle'
+import { useBasket } from '@/hooks/useCDP'
+import { num } from '@/helpers/num'
 
 const useCDPRedeem = ( ) => {
   const { address } = useWallet()
   const { earnState, setEarnState } = useEarnState()
+  const { data: basket } = useBasket()
+  const { data: prices } = useOraclePrice()
+  
 
   type QueryData = {
     msgs: MsgExecuteContractEncodeObject[] | undefined
@@ -26,16 +32,45 @@ const useCDPRedeem = ( ) => {
     queryKey: [
       'earn_page_management_redeem_msg_creation',
       address,
-      earnState.redeemAmount
+      earnState.redeemAmount,
+      prices,
+      basket
     ],
     queryFn: () => {
       if (!address || !earnState.redeemAmount) return { msgs: undefined }
       var msgs = [] as MsgExecuteContractEncodeObject[]
 
       let messageComposer = new PositionsMsgComposer(address, contracts.cdp)
-      const funds = [{ amount: shiftDigits(earnState.redeemAmount, 6).dp(0).toNumber().toString(), denom: denoms.CDT[0].toString() }]
+      const redemptionAmount = shiftDigits(earnState.redeemAmount, 6).dp(0).toNumber().toString()
+      const funds = [{ amount: redemptionAmount, denom: denoms.CDT[0].toString() }]
       let redeemMsg = messageComposer.redeemCollateral({ maxCollateralPremium: "1" }, funds)
       msgs.push(redeemMsg)
+      
+      /////How many marsUSDC VT tokens will we redeem at a 1% discount using the CDT paid?///
+      //CDT peg price * 99% * cdtAmount = value redeemed
+      const cdtRedemptionPrice = num(basket?.credit_price.price??"0").multipliedBy(0.99)
+      const valueRedeemed = cdtRedemptionPrice.multipliedBy(redemptionAmount)
+
+      //valueRedeemed / marsUSDC VT token price = VT tokens redeemed
+      const vtPrice = parseFloat(prices?.find((price) => price.denom === "factory/osmo1fqcwupyh6s703rn0lkxfx0ch2lyrw6lz4dedecx0y3ced2jq04tq0mva2l/mars-usdc-tokenized")?.price ?? "0")
+      const vtTokensRedeemed = valueRedeemed.dividedBy(vtPrice)
+
+      console.log("vt tokens redeemed", vtTokensRedeemed.toString(), "vt price", vtPrice.toString(), "prices", prices)
+
+      //VT Withdraw Msg
+      const fundsVT = [{ amount: vtTokensRedeemed.toString(), denom: "factory/osmo1fqcwupyh6s703rn0lkxfx0ch2lyrw6lz4dedecx0y3ced2jq04tq0mva2l/mars-usdc-tokenized" }]
+      let withdrawMsg = {
+        typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+        value: MsgExecuteContract.fromPartial({
+        sender: address,
+        contract: contracts.marsUSDCvault,
+        msg: toUtf8(JSON.stringify({
+            exit_vault: {}
+        })),
+        funds: fundsVT
+        })
+      } as MsgExecuteContractEncodeObject
+      msgs.push(withdrawMsg)
       
       return { msgs }
     },
@@ -52,34 +87,7 @@ const useCDPRedeem = ( ) => {
   const onInitialSuccess = () => {
     queryClient.invalidateQueries({ queryKey: ['positions'] })
     queryClient.invalidateQueries({ queryKey: ['useVaultInfo'] })
-
-    //Refetch user balances   
-    queryClient.refetchQueries({ queryKey: ['osmosis balances'] })
-
-    //Use new VT token balances to withdraw from the marsUSDC vault
-    const { data: balances } = useBalance()
-    const vtBalance = balances.find((b: any) => b.denom === "factory/osmo1fqcwupyh6s703rn0lkxfx0ch2lyrw6lz4dedecx0y3ced2jq04tq0mva2l/mars-usdc-tokenized")?.amount
-    // VT Withdraw Msg
-    const funds = [{ amount: vtBalance.toString(), denom: "factory/osmo1fqcwupyh6s703rn0lkxfx0ch2lyrw6lz4dedecx0y3ced2jq04tq0mva2l/mars-usdc-tokenized" }]
-    const withdrawVTMsg  = {
-      typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
-      value: MsgExecuteContract.fromPartial({
-      sender: address,
-      contract: contracts.marsUSDCvault,
-      msg: toUtf8(JSON.stringify({
-          exit_vault: {}
-      })),
-      funds: funds
-      })
-    } as MsgExecuteContractEncodeObject
-    const withdrawVTMsgs = [withdrawVTMsg]
-    const action = useSimulateAndBroadcast({
-      msgs: withdrawVTMsgs,
-      queryKey: ['earn_page_marsUSDC_vault_withdrawal', (withdrawVTMsgs?.toString()??"0")],
-      onSuccess: () => queryClient.invalidateQueries({ queryKey: ['osmosis balances'] }),
-      enabled: !!withdrawVTMsgs,
-    })
-    action.tx.mutate()
+    queryClient.invalidateQueries({ queryKey: ['osmosis balances'] })
   }
 
   return {

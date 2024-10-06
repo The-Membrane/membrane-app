3f09d699e1a6ef6332932d8ab1b2ccf8f1e93c1e
Trix <triccs333@gmail>
2024-10-05 22:15:53 -0400

    update dependencies

--- helpers/parseError.ts
+++ helpers/parseError.ts
@@ -38,7 +38,6 @@ export const parseError = (error: Error) => {
     { regex: /Maximum position number/i, message: "You've reached the max position number for this wallet" },
     { regex: /big.Int: tx parse error/i, message: "Max amount per deposit for this token is 999" },
     { regex: /invalid Uint128/i, message: "Max amount per deposit for this token is 999" },
-
     {
       regex: /Invalid withdrawal, can't leave less than the minimum bid/i,
       message: 'Minimum bid amount is 5 CDT',

80414bec8124195e6ab882a9e5b856dd4274a161
Trix <triccs333@gmail>
2024-10-10 15:08:34 -0400

    test discount query

--- services/cdp.ts
+++ services/cdp.ts
@@ -453,16 +453,18 @@ export const getRiskyPositions = (getRevenue: boolean, basketPositions: BasketPo
   // const tally: number[] = []
   // const totalValue: number[] = []
   
-  const userDiscountQueries = useQueries({
-    queries: basketPositions?.map((basketPosition) =>  ({
-        queryKey: ['user', 'discount', 'cdp', basketPosition.user],
-        queryFn: async () => {
-          console.log(`Fetching discount for address: ${basketPosition.user}`);
-          return getUserDiscountValue(basketPosition.user)
-        },
-        enabled: !!basketPosition.user, // Only run query if address is valid        
-    })),
-  });
+  console.log("user discount", getUserDiscountValue("osmo1fd8z9npe5gd6afm0wj60tryzx04gn5jl84hcm2"))
+  const userDiscountQueries = [] as any[]
+  // const userDiscountQueries = useQueries({
+  //   queries: basketPositions?.map((basketPosition) =>  ({
+  //       queryKey: ['user', 'discount', 'cdp', basketPosition.user],
+  //       queryFn: async () => {
+  //         console.log(`Fetching discount for address: ${basketPosition.user}`);
+  //         return getUserDiscountValue(basketPosition.user)
+  //       },
+  //       enabled: !!basketPosition.user, // Only run query if address is valid        
+  //   })) || [],
+  // });
 
   //Get current LTV & liquidation LTV for all positions
   //Return positions that can be liquidated

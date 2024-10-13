11d06960880b2b803d4fa8dfbba50468c673c016
Trix <triccs333@gmail>
2024-10-13 18:21:10 -0400

    test new APR label button on Home page

--- components/Home/EstimatedAPRLabel.tsx
+++ components/Home/EstimatedAPRLabel.tsx
@@ -10,6 +10,7 @@ function estimatedAPRLabel(){
   const { data: assetPool } = useStabilityAssetPool()
   const stabilityPoolAPR = useMemo(() => {
     if (revenue && assetPool) {
+        console.log("returning", num(revenue.totalExpectedRevenue).dividedBy(assetPool.credit_asset.amount).toFixed(1), revenue.totalExpectedRevenue, assetPool.credit_asset.amount, assetPool)
       return num(revenue.totalExpectedRevenue).dividedBy(assetPool.credit_asset.amount).toFixed(1)
     } else console.log("none of one", revenue, assetPool)
   }, [revenue, assetPool])

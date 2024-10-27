83a491f082eb384e933f3b0e4c2a087c18516198
Trix <triccs333@gmail>
2024-10-27 15:44:46 -0400

    TEST HOME TITLE

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -25,6 +25,8 @@ const Home = React.memo(() => {
             <b>
               <a>E</a><span>X</span><a>P</a><span>E</span><a>R</a><span>I</span><a>M</a><span>E</span><a>N</a><span>T</span><a>A</a><span>L</span>
               &nbsp;
+              <a>S</a><span>T</span><a>A</a><span>B</span><a>L</a><span>E</span><a>C</a><span>O</span><a>I</a><span>N</span>
+              &nbsp;
               <a>V</a><span>A</span><a>U</a><span>L</span><a>T</a><span>S</span>
             </b>
           </h5>

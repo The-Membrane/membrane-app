4e4aaa83a9cf12486175b8956cd66dae843092a5
Trix <triccs333@gmail>
2024-10-27 14:42:16 -0400

    TEST HOME TITLE

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -11,27 +11,30 @@ import useWallet from '@/hooks/useWallet'
 
 const Home = React.memo(() => {
   const isMobile = useBreakpointValue({ base: true, md: false }) ?? false
-  const [sign, setSign] = React.useState("");
+  const [sign, setSign] = React.useState("on");
   
   return (
     <Stack>
       <Stack>
         <StatsCard />
       </Stack>
-      <Stack direction={isMobile ? 'column' : 'row'} justifyContent="center"> 
-        <div onMouseEnter={()=>{setSign("on")}} onMouseLeave={()=>{setSign("")}}>
+      <Stack>        
+        <div onMouseEnter={()=>{setSign("on")}} onMouseLeave={()=>{setSign("on")}}>
           <h5 className={`neonSign${sign}`}>
             <b>
               <a>E</a><span>X</span><a>P</a><span>E</span><a>R</a><span>I</span><a>M</a><span>E</span><a>N</a><span>T</span><a>A</a><span>L</span>
+              &nbsp;
               <a>V</a><span>A</span><a>U</a><span>L</span><a>T</a><span>S</span>
             </b>
           </h5>
         </div>
-        {/* autoSPVault Card */}
-        <SPCard />
-        {/* Earn Vault Card */}
-        <EarnCard />
-        {/* Peg is _? WID Card */}
+        <Stack direction={isMobile ? 'column' : 'row'} justifyContent="center"> 
+          {/* autoSPVault Card */}
+          <SPCard />
+          {/* Earn Vault Card */}
+          <EarnCard />
+          {/* Peg is _? WID Card */}
+        </Stack>      
       </Stack>
     </Stack>
   )

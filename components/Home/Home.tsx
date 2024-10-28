0481ab3694796538ded75d11d06e4722ae04b8d1
Trix <triccs333@gmail>
2024-10-27 21:44:00 -0400

    HOME TITLE

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -25,7 +25,7 @@ const Home = React.memo(() => {
             <b>
               <a>E</a><span>X</span><a>P</a><span>E</span><a>R</a><span>I</span><a>M</a><span>E</span><a>N</a><span>T</span><a>A</a><span>L</span>
               &nbsp;
-              <a>S</a><span>T</span><a>A</a><span>B</span><a>L</a><span>E</span><a>C</a><span>O</span><a>I</a><span>N</span>
+              <a>G</a><span>R</span><a>A</a><span>V</span><a>I</a><span>T</span><a>A</a><span>T</span><a>I</a><span>O</span><a>N</a><span>A</span><a>L</a>
               &nbsp;
               <a>V</a><span>A</span><a>U</a><span>L</span><a>T</a><span>S</span>
             </b>

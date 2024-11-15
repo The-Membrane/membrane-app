02c65ad96a2abaa449d52455342b8e72de4b0a9b
Trix <triccs333@gmail>
2024-11-15 15:45:48 -0500

    Shorten vault tital name

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -24,8 +24,6 @@ const Home = React.memo(() => {
             <b>
               <a>E</a><span>X</span><a>P</a><span>E</span><a>R</a><span>I</span><a>M</a><span>E</span><a>N</a><span>T</span><a>A</a><span>L</span>
               &nbsp;
-              <a>G</a><span>R</span><a>A</a><span>V</span><a>I</a><span>T</span><a>A</a><span>T</span><a>I</a><span>O</span><a>N</a><span>A</span><a>L</a>
-              &nbsp;
               <a>V</a><span>A</span><a>U</a><span>L</span><a>T</a><span>S</span>
             </b>
           </h5>

bf5d12e97b9b2ba63d1af7bc51ac7b311fb3815e
Trix <triccs333@gmail>
2024-11-22 16:42:20 -0500

    test toaster formatting

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -55,7 +55,7 @@ const Home = React.memo(() => {
       //Toast
       toaster.message({
         title: `Position ${positionNum+1}`,
-        message: "Cost:" + num(currentPositionCost).times(100).toFixed(2) + "%\nHealth: " + Math.min(health, 100) + "%",
+        message: "Health: " + Math.min(health, 100) + "%\nCost: " + currentPositionCost,
       })
       //Go to next position
       if (positionNum < totalPositions) {

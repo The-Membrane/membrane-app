070e8eabb2249e4d9d1a2c0cc9c696a64e4a5b5e
Trix <triccs333@gmail>
2024-11-27 18:23:34 -0500

    reset rate calc

--- hooks/useToaster.tsx
+++ hooks/useToaster.tsx
@@ -130,7 +130,7 @@ const useToaster = (): IToaster => {
       description: <ToastContent explorer={_explorer} message={message} />,
       status: ToastTypes.Info,
       position: 'top-right',
-      duration: 5000,
+      duration: 7000,
     })
   }
 

ba7d4f551a08d669239db0f853d5e5faf2e8d51c
Trix <triccs333@gmail>
2025-01-03 16:23:56 -0500

    test modal

--- components/TxError.tsx
+++ components/TxError.tsx
@@ -9,12 +9,13 @@ type Props = TextProps & {
 
 const TxError = ({ action, ...textProps }: Props) => {
   const { isError, error } = action?.simulate || {}
-  if (!isError) return null
-
+  
   const errorMessage = useMemo(() => {
     if (!error) return null
     return parseError(error)
   }, [error])
+
+  if (!isError) return null
 
   return (
     <Text fontSize="sm" color="red.500" {...textProps}>
@@ -22,5 +23,4 @@ const TxError = ({ action, ...textProps }: Props) => {
     </Text>
   )
 }
-
 export default TxError

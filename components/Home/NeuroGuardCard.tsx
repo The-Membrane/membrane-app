643edd8bf0b846ddd3f79f19a4cedf492474f7e5
Trix <triccs333@gmail>
2025-01-07 12:15:43 -0500

    format & test balance undefined handling

--- components/Home/NeuroGuardCard.tsx
+++ components/Home/NeuroGuardCard.tsx
@@ -119,7 +119,7 @@ const NeuroGuardOpenEntry = React.memo(({
   const minValue = ((21 / ((asset.maxBorrowLTV ?? 0) * 0.8)) + 1)
   const minAmount = num(minValue).dividedBy(asset.price ?? 0).toNumber()
   {/* @ts-ignore */ }
-  const isDisabled = minAmount > asset.balance
+  const isDisabled = asset ? minAmount > asset.balance : false
 
 
   const cost = basketAssets.find((basketAsset) => basketAsset?.asset?.base === asset.base)?.interestRate || 0
@@ -137,7 +137,7 @@ const NeuroGuardOpenEntry = React.memo(({
         </HStack>
         <Text width="25%" justifyContent="left" variant="title" textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
           {/* @ts-ignore */}
-          {num(asset.balance).toFixed(2)}
+          {num((asset.balance ?? 0)).toFixed(2)}
         </Text>
         <Text width="25%" justifyContent="left" variant="title" textAlign="center" fontSize="lg" letterSpacing="1px" display="flex" >
           {yieldValue}%
@@ -204,7 +204,7 @@ const NeuroGuardExistingEntry = React.memo(({
   }, [])
 
   {/* @ts-ignore */ }
-  const isDisabled = asset.balance === 0
+  const isDisabled = asset.balance == 0
   const yieldValue = Math.max(num(RBYield).times(guardedPosition.LTV).minus(guardedPosition.cost).times(100).toNumber(), 0).toFixed(1)
 
   return (
@@ -225,7 +225,7 @@ const NeuroGuardExistingEntry = React.memo(({
         <Text width="20%" justifyContent="left" variant="title" textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
           N/A
         </Text>
-        <HStack width={"20%"} gap="0">
+        <HStack width={"36%"}>
           <NeuroDepositModal isOpen={isDepositOpen} onClose={toggleDepositOpen} asset={asset?.base ?? ""} position_id={guardedPosition.position.position_id} >
             <Button
               width="100%"

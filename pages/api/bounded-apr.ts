56b21e2b8cc6af323074df2000cece81f1b18643
Trix <triccs333@gmail>
2025-01-19 13:52:28 -0500

    test api for apr

--- pages/api/bounded-apr.ts
+++ pages/api/bounded-apr.ts
@@ -16,38 +16,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       res.setHeader('Allow', ['GET']);
       return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
     }
-
-    const useDiscounts = true;
-    //req.query.useDiscounts === 'true';
-
-    const allPositions = await getBasketPositions();
-    const basket = await getBasket();
+    const [allPositions, basket, interest, vaultTVL] = await Promise.all([
+      getBasketPositions(),
+      getBasket(),
+      getCollateralInterest(),
+      getBoundedTVL()
+    ]);
     const prices = await getOraclePrices(basket);
-    const interest = await getCollateralInterest();
-    const vaultTVL = await getBoundedTVL();
+
 
     if (!prices || !allPositions || !basket || !interest || !vaultTVL) {
       return res.status(500).json({ error: 'Failed to fetch required data.' });
     }
 
-    const userDiscountPromises = useDiscounts
-      ? allPositions.map(async (basketPosition) => {
-        const totalCredit = basketPosition.positions.reduce(
-          (acc, position) => acc + parseInt(position.credit_amount, 10),
-          0
-        );
-        if (totalCredit <= 1000) return { discount: 0 };
-        return getUserDiscount(basketPosition.user);
-      })
-      : [];
-
-    const userDiscounts = await Promise.allSettled(userDiscountPromises);
-
-    const userDiscountResults = userDiscounts.map((result) =>
-      result.status === 'fulfilled' ? result.value : { discount: 0 }
-    );
-
-    const cdpCalcs = getEstimatedAnnualInterest(allPositions, prices, basket, interest, userDiscountResults);
+    const cdpCalcs = getEstimatedAnnualInterest(allPositions, prices, basket, interest, []);
 
     const apr = num(cdpCalcs.totalExpectedRevenue)
       .times(0.80)

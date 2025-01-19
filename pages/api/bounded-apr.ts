c1d3b30b2426aad1621f43027d23075047970ec9
Trix <triccs333@gmail>
2025-01-19 14:02:37 -0500

    time api for apr

--- pages/api/bounded-apr.ts
+++ pages/api/bounded-apr.ts
@@ -16,6 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       res.setHeader('Allow', ['GET']);
       return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
     }
+    console.time("bulk queries");
     const [allPositions, basket, interest, vaultTVL] = await Promise.all([
       getBasketPositions(),
       getBasket(),
@@ -23,13 +24,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       getBoundedTVL()
     ]);
     const prices = await getOraclePrices(basket);
+    console.timeEnd("bulk queries");
 
 
     if (!prices || !allPositions || !basket || !interest || !vaultTVL) {
       return res.status(500).json({ error: 'Failed to fetch required data.' });
     }
 
+    console.time("getEstimatedAnnualInterest");
     const cdpCalcs = getEstimatedAnnualInterest(allPositions, prices, basket, interest, []);
+    console.timeEnd("getEstimatedAnnualInterest");
 
     const apr = num(cdpCalcs.totalExpectedRevenue)
       .times(0.80)

2e43577fff24f13540f18fd69e57b1b5ccb618a7
Trix <triccs333@gmail>
2025-01-20 17:38:38 -0500

    update liq logic

--- services/cdp.ts
+++ services/cdp.ts
@@ -536,7 +536,7 @@ export const getRiskyPositions = (basketPositions: BasketPositionsResponse[], pr
           if (debt === 0) { console.log("no debt"); return undefined }
           ////////////////////////////////
           const debtValue = num(debt).times(basket.credit_price.price).toNumber()
-          if (debt === 0) { console.log("no debt"); return undefined }
+          if (debtValue === 0) { console.log("no debt"); return undefined }
           const ltv = getLTV(tvl, debtValue)
           const positionsWithRatio = getAssetRatio(false, tvl, positions)
           const basketAssets = getBasketAssets(basket!, interest!)

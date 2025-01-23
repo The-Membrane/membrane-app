b1ecb2c85ea33b223fe6e13abb25895ed9337a00
Trix <triccs333@gmail>
2025-01-23 13:53:18 -0500

    test load with stats back up

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -9,65 +9,68 @@ import { useOraclePrice } from '@/hooks/useOracle'
 import OnboardModal from './Home/LeapOnboarding'
 
 
-// export const Stats = React.memo(({ label, value }) => (
-//   <Stack gap="1">
-//     <Text variant="title" letterSpacing="unset" textTransform="none"
-//       textShadow="0px 0px 8px rgba(223, 140, 252, 0.80)" fontSize="4xl">
-//       {label}
-//     </Text>
-//     <Text variant="title" letterSpacing="unset"
-//       textShadow="0px 0px 8px rgba(223, 140, 252, 0.80)" fontSize="4xl">
-//       {value}
-//     </Text>
-//   </Stack>
-// ))
+export const Stats = React.memo(({ label, value }) => (
+  <Stack gap="1">
+    <Text variant="title" letterSpacing="unset" textTransform="none"
+      textShadow="0px 0px 8px rgba(223, 140, 252, 0.80)" fontSize="4xl">
+      {label}
+    </Text>
+    <Text variant="title" letterSpacing="unset"
+      textShadow="0px 0px 8px rgba(223, 140, 252, 0.80)" fontSize="4xl">
+      {value}
+    </Text>
+  </Stack>
+))
 
 export const StatsCard = React.memo(() => {
-  // const tvl = useMemo(() =>
-  //   getProjectTVL({ basket, prices })
-  //   , [basket, prices])
+  const { data: basket } = useBasket()
+  const { data: prices } = useOraclePrice()
 
-  // const mintedAmount = useMemo(() => {
-  //   const cdtAmount = basket?.credit_asset?.amount || 0
-  //   return shiftDigits(cdtAmount, -6).dp(0).toNumber()
-  // }, [basket])
+  const tvl = useMemo(() =>
+    getProjectTVL({ basket, prices })
+    , [basket, prices])
 
-  // const [titleToggle, setTitleToggle] = useState(true)
+  const mintedAmount = useMemo(() => {
+    const cdtAmount = basket?.credit_asset?.amount || 0
+    return shiftDigits(cdtAmount, -6).dp(0).toNumber()
+  }, [basket])
+
+  const [titleToggle, setTitleToggle] = useState(true)
   const [isOpen, setOpen] = useState(false)
 
-  // const onTitleClick = useCallback(() =>
-  //   setTitleToggle(prev => !prev)
-  //   , [])
+  const onTitleClick = useCallback(() =>
+    setTitleToggle(prev => !prev)
+    , [])
 
   return (
     <Stack gap={5}>
       <HStack mt="auto" gap="24" justifyContent="center">
-        {/* {titleToggle ? ( */}
-        <Stack gap={5}>
+        {titleToggle ? (
+          <Stack gap={5}>
 
-          <h1
-            className={"home-title"}
-          >
-            Decentralized Stablecoins: A New Age of Empowerment
-          </h1>
-          <Button alignSelf="center" width="31%" minWidth="180px"
-            onClick={() => setOpen(true)}>
-            Add Funds to Osmosis
-          </Button>
-        </Stack>
-        {/* ) : ( */}
+            <h1
+              className={"home-title"}
+            >
+              Decentralized Stablecoins: A New Age of Empowerment
+            </h1>
+            <Button alignSelf="center" width="31%" minWidth="180px"
+              onClick={() => setOpen(true)}>
+              Add Funds to Osmosis
+            </Button>
+          </Stack>
+        ) : (
 
 
-        {/* <Text onClick={onTitleClick} fontWeight="bold" fontSize="33px"
-            fontFamily="monospace" justifyContent="center"
-            display="flex" textAlign="center">
-            welcome within.
-          </Text>
-          // <HStack gap={16} onClick={onTitleClick}>
-          //   <Stats label="TVL" value={Formatter.currency(tvl, 0)} />
-          //   <Stats label="Total Minted" value={`${Formatter.tvl(mintedAmount)} CDT`} />
-          // </HStack>
-        )} */}
+          // <Text onClick={onTitleClick} fontWeight="bold" fontSize="33px"
+          //     fontFamily="monospace" justifyContent="center"
+          //     display="flex" textAlign="center">
+          //     welcome within.
+          //   </Text>
+          <HStack gap={16} onClick={onTitleClick}>
+            <Stats label="TVL" value={Formatter.currency(tvl, 0)} />
+            <Stats label="Total Minted" value={`${Formatter.tvl(mintedAmount)} CDT`} />
+          </HStack>
+        )}
       </HStack>
       <OnboardModal isOpen={isOpen} setOpen={setOpen} />
       <Divider mx="0" mb="5" />

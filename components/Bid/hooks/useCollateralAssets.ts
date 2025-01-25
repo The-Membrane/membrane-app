7cf4d3c02c88e10431662d4fafdfc79042e5b236
triccs <ebmichigan99@gmail.com>
2025-01-24 22:56:28 -0800

    test useCollateral timer: was 20-40ms last time

--- components/Bid/hooks/useCollateralAssets.ts
+++ components/Bid/hooks/useCollateralAssets.ts
@@ -1,22 +1,22 @@
 import { getAssetByDenom, getAssets } from '@/helpers/chain'
+import { useAssetBySymbol } from '@/hooks/useAssets'
 import { useBasket } from '@/hooks/useCDP'
 import { useMemo } from 'react'
 
 const useCollateralAssets = () => {
   const { data: basket } = useBasket()
 
-  const assets = getAssets("osmosis")
 
   return useMemo(() => {
     return basket?.collateral_types
       ?.map(({ asset, max_borrow_LTV }) => {
         const denom = asset?.info?.native_token?.denom || asset?.info?.token?.address
-        const newAsset = assets?.find((asset) => asset.base === denom)
+        const newAsset = useAssetBySymbol(denom)
         if (!newAsset) return null
         return { ...newAsset, maxBorrowLTV: max_borrow_LTV }
       })
       .filter((asset) => !!asset)
-  }, [basket, assets])
+  }, [basket])
 }
 
 export default useCollateralAssets

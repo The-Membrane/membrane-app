fab525f7c3bbe14387c21e2afe0eff110578d17b
triccs <ebmichigan99@gmail.com>
2025-02-01 17:23:23 -0800

    test numia api

--- services/numia.ts
+++ services/numia.ts
@@ -15,6 +15,6 @@ const requestOptions: RequestInit = {
 export const getCDTDailyVolume = async () => {
     const data = await fetch("https://osmosis.numia.xyz/tokens/v2/factory/osmo1s794h9rxggytja3a4pmwul53u98k06zy2qtrdvjnfuxruh7s8yjs6cyxgd/ucdt", requestOptions)
     const json = await data.json()
-    console.log(json)
+    console.log("numia log", json)
     return json as Promise<any>
 }

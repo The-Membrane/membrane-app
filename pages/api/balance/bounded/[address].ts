c8c9c801b019a25c3c5df8a1e5c6d3ee7bada77a
triccs <ebmichigan99@gmail.com>
2025-02-03 13:33:27 -0800

    fix balance api

--- pages/api/balance/bounded/[address].ts
+++ pages/api/balance/bounded/[address].ts
@@ -1,12 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { denoms } from '@/config/defaults';
 import { Asset, getAssets } from '@/helpers/chain';
 import { num, shiftDigits } from '@/helpers/num';
+import { getBasket } from '@/services/cdp';
 import { getBoundedUnderlyingCDT } from '@/services/earn';
+import { getOraclePrices } from '@/services/oracle';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { osmosis } from 'osmojs';
 
 type Data = {
-    balance?: number,
+    strategy?: string,
+    balance?: {
+        amount: string,
+        usd: number
+    },
+    unclaimed_rewards?: {
+        total_usd: number,
+    }
     error?: string
 }
 
@@ -46,14 +56,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         }
         const address = req.query.address as string
 
-        const [assets, balances] = await Promise.all([
+        const [assets, balances, basket] = await Promise.all([
             getAssets(),
-            getBalances(address)
+            getBalances(address),
+            getBasket()
         ]);
 
-        if (!assets || !balances) {
-            return res.status(500).json({ error: 'Failed to fetch asset or balances.' });
+        if (!assets || !balances || !basket) {
+            return res.status(500).json({ error: 'Failed to fetch asset, basket or balances.' });
         }
+
+        const prices = await getOraclePrices(basket)
+
+        if (!prices) {
+            return res.status(500).json({ error: 'Failed to fetch prices.' });
+        }
+
+        const cdtMarketPrice = prices?.find((price) => price.denom === denoms.CDT[0])?.price || basket?.credit_price.price || "1"
 
         const boundCDTAsset = assets.find((asset) => asset.symbol === 'range-bound-CDT') as Asset
         const boundCDTBalance = await getBalanceByAsset(balances, address, boundCDTAsset) ?? "1"
@@ -66,7 +85,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         const underlyingCDT = Number(shiftDigits(underlyingData, -6)) ?? 0
 
         return res.status(200).json({
-            balance: underlyingCDT
+            strategy: "cdt-usdc-cl-lp-vault",
+            balance: {
+                amount: underlyingCDT.toString(),
+                usd: num(underlyingCDT).times(cdtMarketPrice).toNumber()
+            },
+            unclaimed_rewards: {
+                total_usd: 0
+            }
         });
     } catch (error) {
         console.error('API Error:', error);

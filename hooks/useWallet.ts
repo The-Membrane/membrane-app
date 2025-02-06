f41cfc27adcc616bc0cc83bb9080bc95ad8a6b47
triccs <ebmichigan99@gmail.com>
2025-02-05 16:35:24 -0800

    remove hardcoded wallet

--- hooks/useWallet.ts
+++ hooks/useWallet.ts
@@ -1,7 +1,7 @@
 import { useChain } from '@cosmos-kit/react'
 
 const useWallet = (chainID: string = "osmosis") => {
-  return { ...useChain(chainID), address: "osmo15qn0rc5p42q9vzx4d99xwfl68pwd8pyln7ds2l" }
+  return useChain(chainID)
 }
 
 export default useWallet

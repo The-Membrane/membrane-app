c737e95470ae80e8aeaaa84280651556902b7c18
triccs <ebmichigan99@gmail.com>
2025-02-06 12:31:57 -0800

    remove

--- components/Earn/Earn.tsx
+++ components/Earn/Earn.tsx
@@ -1,16 +1,13 @@
 import { HStack, Stack, useBreakpointValue } from '@chakra-ui/react'
 import React from "react"
 import Deposit from './Deposit'
-import RangeBoundLPCard from '../Home/RangeBoundLPCard'
 
 // export const FOUR_WEEK_TREASURY_YIELD = 0.0529;
 
 const Earn = React.memo(() => {
-  const isMobile = useBreakpointValue({ base: true, md: false })
   return (
     <HStack display={"inline"}>
       <Deposit />
-      <RangeBoundLPCard />
     </HStack>
   )
 })

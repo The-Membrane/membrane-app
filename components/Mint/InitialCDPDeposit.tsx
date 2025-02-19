ee6ca9fe00e35c997f24eec30957e733a338c7b0
triccs <ebmichigan99@gmail.com>
2025-02-19 14:13:36 -0800

    reformat ossified label

--- components/Mint/InitialCDPDeposit.tsx
+++ components/Mint/InitialCDPDeposit.tsx
@@ -41,12 +41,12 @@ export const InitialCDPDeposit = () => {
 
 
     const handleTransaction = (transactionType: string, transactionValue: number) => {
-        console.log("transactionType", transactionType, transactionValue)
+        // console.log("transactionType", transactionType, transactionValue)
         if (!transactionType || transactionValue <= 0) return;
 
         let updatedAssets = mintState.assets.map((a) => {
             if (a.symbol !== selectedAsset?.symbol) return a;
-            console.log("asset made it thru", a)
+            // console.log("asset made it thru", a)
 
             const sliderValue = transactionType === "deposit" ? Number(transactionValue) : -Number(transactionValue);
 
@@ -55,7 +55,7 @@ export const InitialCDPDeposit = () => {
             const amountValue = num(diffInUsd).isGreaterThan(selectedAsset.depositUsdValue)
                 ? newDeposit
                 : -diffInUsd
-            console.log("asset stats", selectedAsset.depositUsdValue, sliderValue, diffInUsd, newDeposit, amountValue)
+            // console.log("asset stats", selectedAsset.depositUsdValue, sliderValue, diffInUsd, newDeposit, amountValue)
             const amount = num(amountValue).dividedBy(selectedAsset.price).dp(selectedAsset.decimal ?? 6).toNumber()
             //
             //
@@ -79,72 +79,66 @@ export const InitialCDPDeposit = () => {
     console.log("selectedAsset", selectedAsset);
 
     //TODO:
-    //- Create Add Asset that converts the current action into a label with an Edit button above the new action
+    //- Add per asset Max limiter to input
+    //- Make reset button work for ossified labels
+    //- Make asset button correctly dynamic for deposits/withdraws & mint/repay combos
+    //- Click to edit the ossified labeled assets
 
-
-    //- Remove redemption card & swap it into a action queue list
     //- Place redemption card in a modal somewhere
 
     return (
         <Stack>
             <Stack>
-                {ossifiedDeposits.map((asset) => {
-                    if (!asset || asset.amountValue === 0) return null;
-                    return (
-                        <Card width="80%" boxShadow={"0 0 25px rgba(90, 90, 90, 0.5)"} paddingY={0}>
-                            <HStack>
-                                <Text variant="title" textTransform={"none"} textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
-                                    Depositing {asset.amountValue} {asset?.symbol}
-                                </Text>
-                            </HStack>
-                            {/* <MintInput
-                                asset={asset}
-                                handleTransaction={handleTransaction}
-                                transactionValue={transactionValue}
-                                setTransactionValue={setTransactionValue}
-                            /> */}
-                        </Card>
-                    )
-                })}
-                <div style={{ width: "20%", alignSelf: "center" }}><Select options={assetsWithOptions} onChange={onChange} value={selectedAsset} /></div>
-                <HStack mt="5%" width="100%" justifyContent="left">
-                    <HStack width="75%">
-                        {selectedAsset && selectedAsset.logo && <Image src={selectedAsset?.logo} w="30px" h="30px" />}
-                        <Text variant="title" textTransform={"none"} textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
-                            Deposit {selectedAsset?.symbol}
+                {ossifiedDeposits && ossifiedDeposits.length > 0 && (
+                    <HStack>
+                        <Text variant="title" textTransform="none" textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
+                            Depositing {ossifiedDeposits
+                                .filter(asset => asset && asset.amountValue > 0 && asset.txType === "deposit")
+                                .map(asset => `${Number(asset.amountValue).toFixed(2)} ${asset.symbol}`)
+                                .join(", ")}
                         </Text>
                     </HStack>
-                </HStack>
-                <Input
-                    width={"100%"}
-                    textAlign={"right"}
-                    placeholder="Enter Amount"
-                    type="number"
-                    variant={"ghost"}
-                    value={Number(transactionValue).toFixed(2)}
-                    max={selectedAsset?.walletsdValue}
-                    onChange={(e) => { e.preventDefault(); setTransactionValue(e.target.value); handleTransaction("deposit", Number(e.target.value)) }}
-                />
-                <HStack alignContent={"right"} width={"100%"} justifyContent={"right"}>
-                    <Button
-                        onClick={() => { setTransactionValue(selectedAsset?.walletsdValue.toString() ?? "0"); handleTransaction("deposit", selectedAsset?.walletsdValue ?? 0) }}
-                        width="10%" variant="unstyled" fontWeight="normal"
-                    >
-                        <Text
-                            variant="body"
-                            justifySelf={"center"}
-                            textTransform="none"
-                            fontSize="sm"
-                            letterSpacing="1px"
-                            display="flex">
-                            max
-                        </Text>
-                    </Button>
-                </HStack>
+                )}
+
+                {assetsWithOptions && assetsWithOptions.length != 0 && <><div style={{ width: "20%", alignSelf: "center" }}><Select options={assetsWithOptions} onChange={onChange} value={selectedAsset} /></div>
+                    <HStack mt="5%" width="100%" justifyContent="left">
+                        <HStack width="75%">
+                            {selectedAsset && selectedAsset.logo && <Image src={selectedAsset?.logo} w="30px" h="30px" />}
+                            <Text variant="title" textTransform={"none"} textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
+                                Deposit {selectedAsset?.symbol}
+                            </Text>
+                        </HStack>
+                    </HStack>
+                    <Input
+                        width={"100%"}
+                        textAlign={"right"}
+                        placeholder="Enter Amount"
+                        type="number"
+                        variant={"ghost"}
+                        value={Number(transactionValue).toFixed(2)}
+                        max={selectedAsset?.walletsdValue}
+                        onChange={(e) => { e.preventDefault(); setTransactionValue(e.target.value); handleTransaction("deposit", Number(e.target.value)) }}
+                    />
+                    <HStack alignContent={"right"} width={"100%"} justifyContent={"right"}>
+                        <Button
+                            onClick={() => { setTransactionValue(selectedAsset?.walletsdValue.toString() ?? "0"); handleTransaction("deposit", selectedAsset?.walletsdValue ?? 0) }}
+                            width="10%" variant="unstyled" fontWeight="normal"
+                        >
+                            <Text
+                                variant="body"
+                                justifySelf={"center"}
+                                textTransform="none"
+                                fontSize="sm"
+                                letterSpacing="1px"
+                                display="flex">
+                                max
+                            </Text>
+                        </Button>
+                    </HStack></>}
                 {/* On click, ossify the current deposit asset & open a new deposit section */}
                 {selectedAsset && assetsWithOptions && assetsWithOptions.length != 0 && <Button
                     alignSelf="center"
-                    onClick={() => { setOssifiedDeposits([...ossifiedDeposits, { ...selectedAsset, amountValue: transactionValue }]); setSelectedAsset(undefined) }}
+                    onClick={() => { setOssifiedDeposits([...ossifiedDeposits, { ...selectedAsset, amountValue: transactionValue, txType: "deposit" }]); setSelectedAsset(undefined) }}
                     width={"30%"}
                     isDisabled={Number(transactionValue) === 0}
                     fontFamily="Inter"

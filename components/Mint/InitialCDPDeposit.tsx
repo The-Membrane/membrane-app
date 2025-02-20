a1056bafc00c63e445a2de4028bcab029846ffe8
triccs <ebmichigan99@gmail.com>
2025-02-20 13:21:42 -0800

    test dynamic current action label

--- components/Mint/InitialCDPDeposit.tsx
+++ components/Mint/InitialCDPDeposit.tsx
@@ -99,10 +99,15 @@ export const InitialCDPDeposit = () => {
                     <Stack>
                         {ossifiedDeposits && ossifiedDeposits.length > 0 && (
                             <Text variant="title" textTransform="none" textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
-                                Depositing {ossifiedDeposits
+                                Depositing {selectedAsset ? ossifiedDeposits.concat([selectedAsset])
                                     .filter(asset => asset && asset.amountValue > 0 && asset.txType === "deposit")
                                     .map(asset => `${Number(asset.amountValue).toFixed(2)} ${asset.symbol}`)
-                                    .join(", ")}
+                                    .join(", ")
+                                    : ossifiedDeposits
+                                        .filter(asset => asset && asset.amountValue > 0 && asset.txType === "deposit")
+                                        .map(asset => `${Number(asset.amountValue).toFixed(2)} ${asset.symbol}`)
+                                        .join(", ")
+                                }
                             </Text>)}
                         {mintState.newDebtAmount && mintState.newDebtAmount != 0 && <Text variant="title" textTransform="none" textAlign="center" fontSize="lg" letterSpacing="1px" display="flex">
                             Borrowing {mintState.newDebtAmount} CDT

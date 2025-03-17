18baa6dbe119c7f9317099c305e516462d96f6f6
triccs <ebmichigan99@gmail.com>
2025-03-17 10:50:10 -0700

    fix labels

--- components/Home/RangeBoundVisual.tsx
+++ components/Home/RangeBoundVisual.tsx
@@ -145,88 +145,86 @@ const RangeBoundVisual = () => {
       <Text alignSelf="center" fontFamily="Inter" fontSize="xl" fontWeight={"bold"}> TVL: ${(num(shiftDigits(TVL ?? "0", -6)).times(cdtPrice).toFixed(2))}</Text>
       <Text alignSelf="center" fontFamily="Inter" fontSize="xl" fontWeight={"bold"}> {shiftDigits(existingBuffer ?? "0", -6)} CDT Waiting to Sell High</Text>
 
-      <Stack height="100%">
 
 
-        <HStack width="100%" height="100%" gap={0}>
-          {/* Tick marks container */}
-          <Box position="relative" height="100%" width="40px" mr={2}>
-            {ticks.map((tick) => (
+      <HStack width="100%" height="100%" gap={0}>
+        {/* Tick marks container */}
+        <Box position="relative" height="100%" width="40px" mr={2}>
+          {ticks.map((tick) => (
+            <Box
+              key={tick.value}
+              position="absolute"
+              left="0"
+              top={`${(1 - (tick.value - 0.988) / (1.001 - 0.988)) * 100}%`}
+              transform="translateY(-50%)"
+              // width="100%"
+              display="flex"
+              alignItems="center"
+              gap={2}
+            >
               <Box
-                key={tick.value}
-                position="absolute"
-                left="0"
-                top={`${((tick.value - 0.988) / (1.001 - 0.988)) * 100}%`}
-                transform="translateY(-50%)"
-                // width="100%"
-                display="flex"
-                alignItems="center"
-                gap={2}
+                as="span"
+                color="white"
+                fontSize="sm"
+                width="49px"
+                textAlign="right"
               >
-                <Box
-                  as="span"
-                  color="white"
-                  fontSize="sm"
-                  width="49px"
-                  textAlign="right"
-                >
-                  - {tick.label}
-                </Box>
-                {/* <Box
+                - {tick.label}
+              </Box>
+              {/* <Box
                   width="8px"
                   height="1px"
                   bg="gray.400"
                 /> */}
-              </Box>
-            ))}
-          </Box>
-          <Box height="100%" width="90%" pos="absolute">
-            <Slider
-              value={cdtPrice}
-              isReadOnly
-              cursor="default"
-              min={0.988}
-              max={1.001}
-              orientation="vertical"
-              width="inherit"
-              pos="relative"
-              height="100%"
+            </Box>
+          ))}
+        </Box>
+        <Box height="100%" width="90%" pos="absolute">
+          <Slider
+            value={cdtPrice}
+            isReadOnly
+            cursor="default"
+            min={0.988}
+            max={1.001}
+            orientation="vertical"
+            width="inherit"
+            pos="relative"
+            height="100%"
+          >
+            <SliderTrack className="noLeftHand" height="100%" left="0%" h="1.5">
+            </SliderTrack>
+            <SliderThumb
+              transform="0"
+              left="0px"
+              h="5px"
+              w="95%"
+              borderRadius="2px"
+              bg={colors.sliderThumb}
+              _hover={{ bg: "blue.600" }}
             >
-              <SliderTrack className="noLeftHand" height="100%" left="0%" h="1.5">
-              </SliderTrack>
-              <SliderThumb
-                transform="0"
-                left="0px"
-                h="5px"
-                w="95%"
-                borderRadius="2px"
-                bg={colors.sliderThumb}
-                _hover={{ bg: "blue.600" }}
-              >
-                <Box position="relative" left="57%" color="white" p="1" borderRadius="md" fontSize="lg" whiteSpace="nowrap">
-                  ${cdtPrice.toFixed(4)}
-                </Box>
-              </SliderThumb>
-            </Slider>
-          </Box>
+              <Box position="relative" left="57%" color="white" p="1" borderRadius="md" fontSize="lg" whiteSpace="nowrap">
+                ${cdtPrice.toFixed(4)}
+              </Box>
+            </SliderThumb>
+          </Slider>
+        </Box>
 
-          <Stack justifyContent="center" gap="33%" width="100%" height="100%">
-            <PriceBox
-              switch={cSwitch}
-              setSwitch={setCSwitch}
-              isTop={true}
-              tvl={positions?.positionsTVL.ceilingTVL.toFixed(2)}
-              range="$1.00 - $0.9999"
-            />
-            <PriceBox
-              switch={fSwitch}
-              setSwitch={setFSwitch}
-              tvl={positions?.positionsTVL.floorTVL.toFixed(2)}
-              range="$0.990 - $0.9899"
-            />
-          </Stack>
-        </HStack>
-      </Stack>
+        <Stack justifyContent="center" gap="33%" width="100%" height="100%">
+          <PriceBox
+            switch={cSwitch}
+            setSwitch={setCSwitch}
+            isTop={true}
+            tvl={positions?.positionsTVL.ceilingTVL.toFixed(2)}
+            range="$1.00 - $0.9999"
+          />
+          <PriceBox
+            switch={fSwitch}
+            setSwitch={setFSwitch}
+            tvl={positions?.positionsTVL.floorTVL.toFixed(2)}
+            range="$0.990 - $0.9899"
+          />
+        </Stack>
+      </HStack>
     </Card>
   )
 }

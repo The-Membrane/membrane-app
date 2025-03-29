0e5fbd0aa70627fca7981db8db4d33ed961dd671
triccs <ebmichigan99@gmail.com>
2025-03-29 12:12:00 -0700

    logs

--- hooks/useEarnQueries.ts
+++ hooks/useEarnQueries.ts
@@ -497,8 +497,8 @@ export const useVaultInfo = () => {
         queryKey: ['useVaultInfo', apr, prices, basket, client, router.pathname],
         queryFn: async () => {
 
-            if (router.pathname != "/manic") return
             console.log("vault info path", router.pathname)
+            if (router.pathname != "/manic") return
             if (!client || !basket || !prices) return
             //Query Vault's CDP 
             const vaultCDPs = await client.getBasketPositions({

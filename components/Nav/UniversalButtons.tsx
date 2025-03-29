6078ddc4beed680e0cc0ef4e4cf9d16d5a82902f
triccs <ebmichigan99@gmail.com>
2025-03-29 10:36:09 -0700

    test side nav closing after simulating

--- components/Nav/UniversalButtons.tsx
+++ components/Nav/UniversalButtons.tsx
@@ -10,18 +10,18 @@ function UniversalButtons({ enabled, setEnabled }: { enabled: boolean, setEnable
     const { action: claim, claims_summary } = useProtocolClaims({ run: enabled })
     const { action: liquidate, liquidating_positions: liq_summ } = useProtocolLiquidations({ run: enabled })
 
-    const claimsDisabled = claims_summary.length === 0 || !enabled
-    const liquidateDisabled = liq_summ.length === 0 || !enabled
+    const claimsDisabled = claims_summary.length === 0
+    const liquidateDisabled = liq_summ.length === 0
 
 
     console.log('uni buttns disabled', liquidateDisabled, claimsDisabled)
 
     useMemo(() => {
-        if (claimsDisabled && liquidateDisabled) {
+        if (claimsDisabled && liquidateDisabled && enabled) {
             console.log("both disabled")
             setEnabled(false)
         }
-    }, [claimsDisabled, liquidateDisabled])
+    }, [claimsDisabled, liquidateDisabled, enabled])
 
     return (
         <Stack as="uniButtons" gap="1">

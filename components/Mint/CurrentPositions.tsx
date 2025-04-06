2560c0343a186823dd23516ba57ccfab33e2b739
triccs <ebmichigan99@gmail.com>
2025-04-06 12:52:24 -0700

    test collapsable position info

--- components/Mint/CurrentPositions.tsx
+++ components/Mint/CurrentPositions.tsx
@@ -1,11 +1,13 @@
-import { HStack, Stack, TabPanel, Card, Text, useBreakpointValue } from '@chakra-ui/react'
+import { HStack, Stack, TabPanel, Card, Text, useBreakpointValue, Box, Collapse, IconButton, useDisclosure } from '@chakra-ui/react'
 import { useCurrentPosition } from './hooks/useCurrentPosition'
 import React from 'react'
 import { colors } from '@/config/defaults'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 
 const CurrentPositions = () => {
   const stats = useCurrentPosition()
   const isMobile = useBreakpointValue({ base: true, xxs: true, xs: true, sm: true, md: true, lg: false })
+  const { isOpen, onToggle } = useDisclosure();
 
   return (
     <Card boxShadow={"0 0 25px rgba(90, 90, 90, 0.5)"} minW="363px" gap="12" h="max-content" px="2" alignSelf={isMobile ? "center" : "undefined"}>
@@ -13,13 +15,26 @@ const CurrentPositions = () => {
         <Text color={stats.health < 10 ? colors.alert : undefined} variant="title" textTransform={"none"} alignSelf="center" fontSize="xl" letterSpacing="1px" display="flex">
           Health: {Math.min(Math.max(0, stats.health), 100)}%
         </Text>
-        {stats.stats.map(({ label, value, textColor = 'white' }) => (
-          <HStack key={label + value} justifyContent="space-between">
-            <Text variant="lable" textTransform={"none"} letterSpacing={0}>{label}</Text>
-            <Text variant="value" color={textColor}>{value}</Text>
-          </HStack>
-        ))}
+
+        <IconButton
+          icon={<ChevronDownIcon />}
+          size="sm"
+          variant="ghost"
+          aria-label="Toggle"
+          transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
+          transition="transform 0.2s"
+        />
+
+        <Collapse in={isOpen} animateOpacity>
+          {stats.stats.map(({ label, value, textColor = 'white' }) => (
+            <HStack key={label + value} justifyContent="space-between">
+              <Text variant="lable" textTransform={"none"} letterSpacing={0}>{label}</Text>
+              <Text variant="value" color={textColor}>{value}</Text>
+            </HStack>
+          ))}
+        </Collapse>
       </Stack>
+
     </Card>
   )
 }

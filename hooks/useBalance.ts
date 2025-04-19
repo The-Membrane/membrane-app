703105c9206b37787eb4c1c4991e9fc22e32f73d
triccs <ebmichigan99@gmail.com>
2025-04-19 12:10:07 -0700

    add balance refresh on window focus

--- hooks/useBalance.ts
+++ hooks/useBalance.ts
@@ -34,6 +34,7 @@ export const useBalance = (chainID: string = "osmosis") => {
     },
     enabled: !!getRpcClient && !!address,
     staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: true,
   })
 }
 
